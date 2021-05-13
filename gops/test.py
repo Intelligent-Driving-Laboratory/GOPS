0b45a1099611dac00f5ebd15f60c46f8846eb4bf
sugardaddy <vcisunhao@163.com>
2021-05-13 18:43:41 +0800

    !23 删除无用文件 * rm files

--- gops/test.py
+++
@@ -1,7 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--env_id', type=str, default='gym_cartpole_conti', help='')
-parser.add_argument('--apprfunc', type=str, default='MLP', help='')
-parser.add_argument('--algorithm', type=str, default='DDPG', help='')
-args = vars(parser.parse_args())
-print(args['env_id'])
