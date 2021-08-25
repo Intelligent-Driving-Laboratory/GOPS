ef2e5145b229a03693f8e3091e45a4bdc3d73d4c
ZhangYuhang <zhang.yuh@outlook.com>
2021-08-25 02:58:36 +0000

    !97 添加带约束的环境 十字路口左转 * add code to fix bugs in beizer * add crossing env data

---
+++ gops/modules/env/gym_crossing_data.py
@@ -0,0 +1,35 @@
+from resources.crossing import endtoend
+
+
+
+class GymCrossingData():
+    def __init__(self):
+        self._env = endtoend.CrossroadEnd2endMixPiFix('left')
+        self.observation_space = self._env.observation_space
+        self.action_space = self._env.action_space
+        self.adv_action_space = None
+
+    def reset(self):
+        s = self._env.reset()
+        return s
+
+    def step(self, action, adv_action=None):
+        return self._env.step(action)
+
+    def render(self, mode='human'):
+        return self._env.render(mode)
+
+    def close(self):
+        self._env.close()
+
+
+
+
+if __name__ == '__main__':
+    env = GymCrossingData()
+    env.reset()
+    for _ in range(100):
+        a = env.action_space.sample()
+        env.step(a)
+        env.render()
+    env.close()
---
+++ gops/modules/env/resources/crossing/__init__.py
@@ -0,0 +1,5 @@
+import gym
+
+from gym.envs.registration import register
+
+# register(id='CrossroadEnd2endMixPiFix-v0', entry_point='env_mix_single.endtoend:CrossroadEnd2endMixPiFix')
