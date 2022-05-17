75a6ac9965fcf0761a35a19a4025d683ba1e1def
gaojiaxin <gaojiaxin2017@163.com>
2022-05-17 16:14:04 +0800

    fhadp add poly example (difficult to convergence unless using fixed data set learning) modify veh2dofconti_data.py, making the sampling data form the same as other environment

--- gops/env/pyth_veh2dofconti_data.py
+++ gops/env/pyth_veh2dofconti_data.py
@@ -4,13 +4,13 @@
 #
 #  Creator: iDLab
 #  Description: Vehicle 3DOF Model
-#  Update Date: 2021-05-55, Congsheng Zhang: create environment
+#  Update Date: 2021-05-55, Jiaxin Gao: create environment
 
 
 from gym import spaces
 import gym
 from gym.utils import seeding
-from gops.env.pyth_veh2dofconti_model import VehicleDynamics, Veh2dofcontiModel
+from gops.env.pyth_veh2dofconti_model import Veh2dofcontiModel
 from gym.wrappers.time_limit import TimeLimit
 from typing import Callable, Dict, List
 import numpy as np
@@ -25,6 +25,117 @@ import sys
 import json
 import os
 
+class VehicleDynamics(object):
+    def __init__(self):
+        self.vehicle_params = dict(C_f=-128915.5,  # front wheel cornering stiffness [N/rad]
+                                   C_r=-85943.6,  # rear wheel cornering stiffness [N/rad]
+                                   a=1.06,  # distance from CG to front axle [m]
+                                   b=1.85,  # distance from CG to rear axle [m]
+                                   mass=1412.,  # mass [kg]
+                                   I_z=1536.7,  # Polar moment of inertia at CG [kg*m^2]
+                                   miu=1.0,  # tire-road friction coefficient
+                                   g=9.81,  # acceleration of gravity [m/s^2]
+                                   )
+        a, b, mass, g = self.vehicle_params['a'], self.vehicle_params['b'], \
+                        self.vehicle_params['mass'], self.vehicle_params['g']
+        F_zf, F_zr = b * mass * g / (a + b), a * mass * g / (a + b)
+        self.vehicle_params.update(dict(F_zf=F_zf,
+                                        F_zr=F_zr))
+        self.path = ReferencePath()
+
+    def f_xu(self, states, actions, tau):
+        A = np.array([[0.4411, -0.6398, 0, 0],
+                      [0.0242, 0.2188, 0, 0],
+                      [0.0703, 0.0171, 1, 2],
+                      [0.0018, 0.0523, 0, 1]])
+        B = np.array([[2.0350], [4.8124], [0.4046], [0.2952]])
+        A = torch.from_numpy(A.astype("float32"))
+        B = torch.from_numpy(B.astype("float32"))
+        v_y, r, delta_y, delta_phi = states[:, 0], states[:, 1], states[:, 2], states[:, 3]
+        next_state = [v_y * A[0, 0] + r * A[0, 1] + delta_y * A[0, 2] + delta_phi * A[0, 3] + B[0, 0] * actions,
+                      v_y * A[1, 0] + r * A[1, 1] + delta_y * A[1, 2] + delta_phi * A[1, 3] + B[1, 0] * actions,
+                      v_y * A[2, 0] + r * A[2, 1] + delta_y * A[2, 2] + delta_phi * A[2, 3] + B[2, 0] * actions,
+                      v_y * A[3, 0] + r * A[3, 1] + delta_y * A[3, 2] + delta_phi * A[3, 3] + B[3, 0] * actions]
+        return torch.stack(next_state, 1)
+
+    def _get_obs(self, veh_states):
+        v_ys, rs, delta_ys, delta_phis = veh_states[:, 0], veh_states[:, 1], veh_states[:, 2], veh_states[:, 3]
+        lists_to_stack = [v_ys, rs, delta_ys, delta_phis]
+        return torch.stack(lists_to_stack, 1)
+
+    def _get_state(self, obses):
+        v_ys, rs, delta_ys, delta_phis = obses[:, 0], obses[:, 1], obses[:, 2], obses[:, 3]
+        lists_to_stack = [v_ys, rs, delta_ys, delta_phis]
+        return torch.stack(lists_to_stack, 1)
+
+    def prediction(self, x_1, u_1, frequency):
+        x_next = self.f_xu(x_1, u_1, 1 / frequency)
+        return x_next
+
+    def simulation(self, states, actions, base_freq):
+        next_states = self.prediction(states, actions, base_freq)
+        v_ys, rs, delta_ys, delta_phis = next_states[:, 0], next_states[:, 1], next_states[:, 2], next_states[:, 3]
+        delta_phis = torch.where(delta_phis > np.pi, delta_phis - 2 * np.pi, delta_phis)
+        delta_phis = torch.where(delta_phis <= -np.pi, delta_phis + 2 * np.pi, delta_phis)
+        next_states = torch.stack([v_ys, rs, delta_ys, delta_phis], 1)
+        return next_states
+
+    def compute_rewards(self, states, actions):  # obses and actions are tensors
+        # veh_state = obs: v_xs, v_ys, rs, delta_ys, delta_phis, xs
+        # veh_full_state: v_xs, v_ys, rs, ys, phis, xs
+        v_ys, rs, delta_ys, delta_phis = states[:, 0], states[:, 1], states[:, 2], \
+                                                   states[:, 3]
+        steers = actions
+        devi_y = -torch.square(delta_ys)
+        devi_phi = -torch.square(delta_phis)
+        punish_yaw_rate = -torch.square(rs)
+        punish_steer = -torch.square(steers)
+        rewards = 0.4 * devi_y + 0.1 * devi_phi + 0.2 * punish_yaw_rate + 0.5 * punish_steer
+        return rewards
+
+class ReferencePath(object):
+    def __init__(self):
+        self.curve_list = [(7.5, 200., 0.), (2.5, 300., 0.), (-5., 400., 0.)]
+        self.period = 1200.
+
+    def compute_path_y(self, x):
+        y = np.zeros_like(x, dtype=np.float32)
+        for curve in self.curve_list:
+            # 正弦的振幅，周期，平移
+            # 这里是对3种正弦曲线进行了叠加。
+            magnitude, T, shift = curve
+            y += magnitude * np.sin((x - shift) * 2 * np.pi / T)
+        return y
+
+    def compute_path_phi(self, x):
+        deriv = np.zeros_like(x, dtype=np.float32)
+        for curve in self.curve_list:
+            magnitude, T, shift = curve
+            deriv += magnitude * 2 * np.pi / T * np.cos(
+                (x - shift) * 2 * np.pi / T)
+        return np.arctan(deriv)
+
+    def compute_y(self, x, delta_y):
+        y_ref = self.compute_path_y(x)
+        return delta_y + y_ref
+
+    def compute_delta_y(self, x, y):
+        y_ref = self.compute_path_y(x)
+        return y - y_ref
+
+    def compute_phi(self, x, delta_phi):
+        phi_ref = self.compute_path_phi(x)
+        phi = delta_phi + phi_ref
+        phi[phi > np.pi] -= 2 * np.pi
+        phi[phi <= -np.pi] += 2 * np.pi
+        return phi
+
+    def compute_delta_phi(self, x, phi):
+        phi_ref = self.compute_path_phi(x)
+        delta_phi = phi - phi_ref
+        delta_phi[delta_phi > np.pi] -= 2 * np.pi
+        delta_phi[delta_phi <= -np.pi] += 2 * np.pi
+        return delta_phi
 
 class SimuVeh2dofconti(gym.Env,):
     def __init__(self, num_future_data=0, num_agent=1, **kwargs):
@@ -78,7 +189,7 @@ class SimuVeh2dofconti(gym.Env,):
         self.veh_state[:, 3] = self.veh_full_state[:, 3] - path_phi
         self.veh_state[:, 2] = self.veh_full_state[:, 2] - path_y
         self.obs = self._get_obs(self.veh_state)
-        return self.obs
+        return self.obs[0]
 
     def step(self, action):  # think of action is in range [-1, 1]
         steer_norm = action
@@ -93,7 +204,7 @@ class SimuVeh2dofconti(gym.Env,):
         self.done = self.judge_done(self.veh_state)
         self.obs = self._get_obs(self.veh_state)
         info = {}
-        return self.obs, reward, self.done, info
+        return self.obs[0], reward, self.done, info
 
     def judge_done(self, veh_state):
         v_ys, rs, delta_ys, delta_phis = veh_state[:, 0], veh_state[:, 1], veh_state[:, 2], \
@@ -120,7 +231,7 @@ def scale_obs(obs):
 
 if __name__=="__main__":
     sys.path.append(r"E:\gops\gops\gops\algorithm")
-    base_dir = r"E:\gops\gops\results\FHADP\220511-185611"
+    base_dir = r"E:\gops\gops\results\FHADP\220517-125614"
     net_dir = os.path.join(base_dir, r"apprfunc\apprfunc_{}.pkl".format(1999))
     parser = argparse.ArgumentParser()
     ################################################
@@ -194,6 +305,7 @@ if __name__=="__main__":
     env = SimuVeh2dofconti()
     model = Veh2dofcontiModel()
     obs = env.reset()
+    obs = torch.from_numpy(np.expand_dims(obs, axis=0).astype("float32"))
     args = vars(parser.parse_args())
     args = init_args(env, **args)
     alg_name = args["algorithm"]
@@ -206,7 +318,6 @@ if __name__=="__main__":
     delta_phi = []
     delta_y = []
     reward_total = []
-    obs = torch.from_numpy(obs.astype("float32"))
     model.reset(obs)
     for _ in range(200):
         batch_obs = scale_obs(obs)
