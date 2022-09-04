490dbe5c502abd3a78647d706bfcaefb2da148b3
yangyujie <yangyj21@mails.tsinghua.edu.cn>
2022-09-04 15:34:30 +0800

    trainer代码规范

--- gops/trainer/off_async_trainer.py
+++ gops/trainer/off_async_trainer.py
@@ -9,26 +9,19 @@
 
 __all__ = ["OffAsyncTrainer"]
 
-import logging
-import queue
+import importlib
 import random
-import threading
 import time
+import warnings
 
-import numpy as np
 import ray
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
 from gops.utils.task_pool import TaskPool
 from gops.utils.tensorboard_tools import add_scalars
-from gops.utils.utils import random_choice_with_index
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
 from gops.utils.tensorboard_tools import tb_tags
-import warnings
-import importlib
+from gops.utils.utils import random_choice_with_index
 
 warnings.filterwarnings("ignore")
 
@@ -39,22 +32,6 @@ class OffAsyncTrainer:
         self.samplers = sampler
         self.buffers = buffer
         self.evaluator = evaluator
-        self.iteration = 0
-        self.replay_batch_size = kwargs["replay_batch_size"]
-        self.max_iteration = kwargs["max_iteration"]
-        self.ini_network_dir = kwargs["ini_network_dir"]
-        self.save_folder = kwargs["save_folder"]
-        self.log_save_interval = kwargs["log_save_interval"]
-        self.apprfunc_save_interval = kwargs["apprfunc_save_interval"]
-        self.save_folder = kwargs["save_folder"]
-        self.log_save_interval = kwargs["log_save_interval"]
-        self.apprfunc_save_interval = kwargs["apprfunc_save_interval"]
-        self.eval_interval = kwargs["eval_interval"]
-        self.sample_interval = 1
-        self.writer = SummaryWriter(log_dir=self.save_folder, flush_secs=20)
-        self.writer.add_scalar(tb_tags["alg_time"], 0, 0)
-        self.writer.add_scalar(tb_tags["sampler_time"], 0, 0)
-        self.writer.flush()
 
         # create center network
         alg_name = kwargs["algorithm"]
@@ -65,17 +42,26 @@ class OffAsyncTrainer:
             raise NotImplementedError("This algorithm does not exist")
         if hasattr(module, alg_name):
             alg_cls = getattr(module, alg_name)
-            alg_net = alg_cls(**kwargs)
+            self.networks = alg_cls(**kwargs)
         else:
             raise NotImplementedError("This algorithm is not properly defined")
 
-        self.networks = alg_net
-
-        self.ini_network_dir = kwargs["ini_network_dir"]
-
         # initialize the networks
-        if self.ini_network_dir is not None:
-            self.networks.load_state_dict(torch.load(self.ini_network_dir))
+        if kwargs["ini_network_dir"] is not None:
+            self.networks.load_state_dict(torch.load(kwargs["ini_network_dir"]))
+
+        self.replay_batch_size = kwargs["replay_batch_size"]
+        self.max_iteration = kwargs["max_iteration"]
+        self.sample_interval = kwargs.get("sample_interval", 1)
+        self.log_save_interval = kwargs["log_save_interval"]
+        self.apprfunc_save_interval = kwargs["apprfunc_save_interval"]
+        self.eval_interval = kwargs["eval_interval"]
+        self.save_folder = kwargs["save_folder"]
+        self.iteration = 0
+
+        self.writer = SummaryWriter(log_dir=self.save_folder, flush_secs=20)
+        add_scalars({"alg_time": 0, "sampler_time": 0}, self.writer, 0)
+        self.writer.flush()
 
         # create sample tasks and pre sampling
         self.sample_tasks = TaskPool()
@@ -83,13 +69,13 @@ class OffAsyncTrainer:
 
         self.warm_size = kwargs["buffer_warm_size"]
         while not all(
-            [
-                l >= self.warm_size
-                for l in ray.get([rb.__len__.remote() for rb in self.buffers])
-            ]
+                [
+                    l >= self.warm_size
+                    for l in ray.get([rb.__len__.remote() for rb in self.buffers])
+                ]
         ):
             for sampler, objID in list(
-                self.sample_tasks.completed()
+                    self.sample_tasks.completed()
             ):
                 batch_data, _ = ray.get(objID)
                 random.choice(self.buffers).add_batch.remote(
@@ -106,7 +92,7 @@ class OffAsyncTrainer:
         self.use_gpu = kwargs["use_gpu"]
         if self.use_gpu:
             for alg in self.algs:
-                alg.to.remote('cuda')
+                alg.to.remote("cuda")
 
         self.start_time = time.time()
 
@@ -145,6 +131,7 @@ class OffAsyncTrainer:
         # learning
         for alg, objID in self.learn_tasks.completed():
             alg_tb_dict, update_info = ray.get(objID)
+
             # replay
             data = ray.get(random.choice(self.buffers).sample_batch.remote(
                 self.replay_batch_size
@@ -152,6 +139,7 @@ class OffAsyncTrainer:
             if self.use_gpu:
                 for k, v in data.items():
                     data[k] = v.cuda()
+
             weights = ray.put(self.networks.state_dict())
             alg.load_state_dict.remote(weights)
             self.learn_tasks.add(
@@ -163,7 +151,9 @@ class OffAsyncTrainer:
                         for i in range(len(v)):
                             update_info[k][i] = v[i].cpu()
             self.networks.remote_update(update_info)
+
             self.iteration += 1
+
             # log
             if self.iteration % self.log_save_interval == 0:
                 print("Iter = ", self.iteration)
@@ -172,12 +162,10 @@ class OffAsyncTrainer:
 
             # evaluate
             if self.iteration % self.eval_interval == 0:
-                # calculate total sample number
                 self.evaluator.load_state_dict.remote(self.networks.state_dict())
                 total_avg_return = ray.get(
                     self.evaluator.run_evaluation.remote(self.iteration)
                 )
-                # get ram for buffer
                 self.writer.add_scalar(
                     tb_tags["Buffer RAM of RL iteration"],
                     sum(ray.get([buffer.__get_RAM__.remote() for buffer in self.buffers])),
@@ -211,20 +199,17 @@ class OffAsyncTrainer:
 
             # save
             if self.iteration % self.apprfunc_save_interval == 0:
-                torch.save(
-                    self.networks.state_dict(),
-                    self.save_folder
-                    + "/apprfunc/apprfunc_{}.pkl".format(self.iteration),
-                )
-
-            if self.iteration == self.max_iteration - 1:
-                torch.save(
-                    self.networks.state_dict(),
-                    self.save_folder
-                    + "/apprfunc/apprfunc_{}.pkl".format(self.iteration),
-                )
-
+                self.save_apprfunc()
 
     def train(self):
         while self.iteration < self.max_iteration:
             self.step()
+
+        self.save_apprfunc()
+        self.writer.flush()
+
+    def save_apprfunc(self):
+        torch.save(
+            self.networks.state_dict(),
+            self.save_folder + "/apprfunc/apprfunc_{}.pkl".format(self.iteration),
+        )
