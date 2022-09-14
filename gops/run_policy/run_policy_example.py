5d816525936eb436af2b38c1f2cb88a99dfca533
wenxuanwang <17888819590@163.com>
2022-09-14 15:33:52 +0800

    Default Changelist

--- gops/run_policy/run_policy_example.py
+++ gops/run_policy/run_policy_example.py
@@ -2,9 +2,8 @@ from policy_runner import PolicyRuner
 
 
 runer = PolicyRuner(
-    log_policy_dir_list=["../../results/PPO/220914-124352"],
-    trained_policy_iteration_list=[100],
-    save_render=False,
-    init_state=[1.0, 1.0, 1.0])
+    log_policy_dir_list=["../../results/SAC/220914-134325"],
+    trained_policy_iteration_list=[12000],
+    save_render=False,)
 
 runer.run()
