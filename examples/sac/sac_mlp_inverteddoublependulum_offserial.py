938fc16210a8dc3786f36721576a63eafb73036e
zyh <zhang,yuh@outlook.com>
2022-09-15 09:45:10 +0800

    update training param

--- examples/sac/sac_mlp_inverteddoublependulum_offserial.py
+++ examples/sac/sac_mlp_inverteddoublependulum_offserial.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
     parser.add_argument("--value_learning_rate", type=float, default=1e-3)
     parser.add_argument("--q_learning_rate", type=float, default=1e-3)
     parser.add_argument("--policy_learning_rate", type=float, default=1e-3)
-    parser.add_argument("--alpha_learning_rate", type=float, default=1e-3)
+    parser.add_argument("--alpha_learning_rate", type=float, default=3e-4)
 
     ################################################
     # 4. Parameters for trainer
@@ -140,7 +140,7 @@ if __name__ == "__main__":
     start_tensorboard(args["save_folder"])
     # Step 1: create algorithm and approximate function
     alg = create_alg(**args)
-    alg.set_parameters({"reward_scale": 0.1, "gamma": 0.99, "tau": 0.2})
+    alg.set_parameters({"reward_scale": 0.1, "gamma": 0.99, "tau": 0.005})
     # Step 2: create sampler in trainer
     sampler = create_sampler(**args)
     # Step 3: create buffer in trainer
