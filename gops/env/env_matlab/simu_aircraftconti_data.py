c934ff5fbf3406cbcfd46258587711a315f41679
wenxuanwang <17888819590@163.com>
2022-09-21 16:45:37 +0800

    1.fix some path errors

--- gops/env/env_matlab/simu_aircraftconti_data.py
+++ gops/env/env_matlab/simu_aircraftconti_data.py
@@ -6,13 +6,12 @@
 #  Description: Aircraft Environment
 #  Update Date: 2021-05-55, Yuhang Zhang: create environment
 
-from gops.env.resources.simu_aircraft_v2.aircraft import GymEnv
-from gops.env.resources.simu_aircraft_v2.aircraft._env import EnvSpec
+from gops.env.env_matlab.resources.simu_aircraft_v2.aircraft import GymEnv
+from gops.env.env_matlab.resources.simu_aircraft_v2.aircraft._env import EnvSpec
 
 from gym import spaces
 import gym
 from gym.utils import seeding
-from gops.env.env_matlab.resources.simu_aircraft import aircraft
 import numpy as np
 
 def env_creator(**kwargs):
@@ -23,6 +22,3 @@ def env_creator(**kwargs):
     return GymEnv(spec)
 
 
-if __name__ == "__main__":
-    from gops.utils.env_utils import random_rollout
-    random_rollout(env_creator())
