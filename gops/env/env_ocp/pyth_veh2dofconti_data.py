a3e52a1beeed56a4477b72db495f3216de428c26
Jiaxin <gaojiaxin2017@163.com>
2022-10-05 15:08:29 +0800

    minor modify

--- gops/env/env_ocp/pyth_veh2dofconti_data.py
+++ gops/env/env_ocp/pyth_veh2dofconti_data.py
@@ -167,11 +167,11 @@ class SimuVeh2dofconti(gym.Env,):
         init_w = None
         obs = None
         if (init_state is None) & (t is None) & (ref_num is None):
-            # flag = [0, 1]
-            # self.ref_num = self.np_random.choice(flag)
-            # t = 20. * self.np_random.uniform(low=0., high=1.)
-            self.ref_num = 1
-            t = 0
+            flag = [0, 1]
+            self.ref_num = self.np_random.choice(flag)
+            t = 20. * self.np_random.uniform(low=0., high=1.)
+            # self.ref_num = 1
+            # t = 0
             self.t = t
             init_delta_y = self.np_random.normal(0, 0.3)
             init_y = self.vehicle_dynamics.compute_path_y(t, self.ref_num) + init_delta_y
