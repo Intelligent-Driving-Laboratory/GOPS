59a92dd591844bfbcef205255fd82299be6299f6
yangyujie <yangyj21@mails.tsinghua.edu.cn>
2022-10-05 14:50:38 +0800

    add env state wrapper

---
+++ gops/env/env_wrapper/state.py
@@ -0,0 +1,29 @@
+import gym
+import numpy as np
+
+
+class StateData(gym.Wrapper):
+    """
+        This wrapper ensures that the environment has a "state" property.
+        If the original environment does not have one, the current observation is returned when calling state.
+    """
+    def __init__(self, env):
+        super(StateData, self).__init__(env)
+        self.current_obs = None
+
+    def reset(self):
+        obs = super(StateData, self).reset()
+        self.current_obs = obs
+        return obs
+
+    def step(self, action):
+        obs, rew, done, info = super(StateData, self).step(action)
+        self.current_obs = obs
+        return obs, rew, done, info
+
+    @property
+    def state(self):
+        if hasattr(self.env, "state"):
+            return np.array(self.env.state, dtype=np.float32)
+        else:
+            return self.current_obs
