b8aef0c771593b88f168a260ca7a1b47d5ad5f47
JieLi <jieli18@163.com>
2022-10-06 10:59:16 +0800

    1. upload control_matrix 2. upload run_aircraftconti_rpi.py

---
+++ gops/sys_simulator/run_aircraftconti_rpi.py
@@ -0,0 +1,15 @@
+from sys_run import PolicyRuner
+
+runer = PolicyRuner(
+    log_policy_dir_list=["../../results/RPI/221005-233132"] * 2,
+    trained_policy_iteration_list=['40', '50'],
+    is_init_state=True,
+    init_state=[1, -1, -1],
+    save_render=False,
+    legend_list=['RPI-40', 'RPI-50'],
+    use_opt=True,
+    constrained_env=False,
+    is_tracking=False,
+    dt=None)
+
+runer.run()
