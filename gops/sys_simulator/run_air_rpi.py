dae27d8303882399094fe07cf183475555b0482d
wenxuanwang <17888819590@163.com>
2022-10-11 09:01:34 +0800

    1. rename some variables

--- gops/sys_simulator/run_air_rpi.py
+++ gops/sys_simulator/run_air_rpi.py
@@ -1,12 +1,12 @@
 from sys_run import PolicyRuner
 
 runer = PolicyRuner(
-    log_policy_dir_list=["../../results/RPI/221005-233433"]*1,
+    log_policy_dir_list=["../../results/RPI/221011-085532"]*1,
     trained_policy_iteration_list=['40'],
     is_init_info=False,
     init_info={"init_state":[0.98, -0.5]},
     save_render=False,
     legend_list=['40'],
-    use_opt=False)
+    use_opt=True)
 
 runer.run()
--- gops/sys_simulator/run_lq_infadp.py
+++ gops/sys_simulator/run_lq_infadp.py
@@ -1,12 +1,12 @@
 from sys_run import PolicyRuner
 
 runer = PolicyRuner(
-    log_policy_dir_list=["../../results/FHADP/221005-224236"]*1,
-    trained_policy_iteration_list=['2000'],
+    log_policy_dir_list=["../../results/FHADP/221011-084859"]*1,
+    trained_policy_iteration_list=['4000'],
     is_init_info=True,
-    init_info={"init_state":[0.98, -0.5]},
+    init_info={"init_state":[2, 1]},
     save_render=False,
-    legend_list=['2000'],
+    legend_list=['4000'],
     use_opt=True)
 
 runer.run()
