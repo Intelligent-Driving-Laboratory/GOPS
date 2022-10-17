5948fcc78f66d45e7cfe6eef449a6cbd20de6987
wenxuanwang <17888819590@163.com>
2022-10-17 20:14:37 +0800

    none

--- gops/sys_simulator/run_lq_infadp.py
+++ gops/sys_simulator/run_lq_infadp.py
@@ -1,12 +1,14 @@
 from sys_run import PolicyRuner
 
 runer = PolicyRuner(
-    log_policy_dir_list=["../../results/FHADP/221011-084859"]*1,
+    log_policy_dir_list=["../../results/FHADP/221017-195545"]*1,
     trained_policy_iteration_list=['4000'],
     is_init_info=True,
-    init_info={"init_state":[2, 1]},
+    init_info={"init_state":[1, 1]},
     save_render=False,
     legend_list=['4000'],
+    plot_range=[0,200],
+    dt=0.05,
     use_opt=True)
 
 runer.run()
