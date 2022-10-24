7bd2dd4e1b6d0f8be863ebe9fab0d7b441922f2b
wenxuanwang <17888819590@163.com>
2022-10-24 12:47:35 +0800

    修改了一些运行示例，更改了mpg以及fhadp的算法接口以适配env_model的要求，修改了cartpoleconti_model里面参数的命名

--- examples/fhadp/fhadp_mlp_veh3dofconti_serial.py
+++ examples/fhadp/fhadp_mlp_veh3dofconti_serial.py
@@ -112,9 +112,6 @@ if __name__ == "__main__":
     start_tensorboard(args["save_folder"])
     # Step 1: create algorithm and approximate function
     alg = create_alg(**args)
-    alg.set_parameters(
-        {"reward_scale": 0.1, "gamma": 0.99, "tau": 0.2, "delay_update": 1}
-    )
     # Step 2: create sampler in trainer
     sampler = create_sampler(**args)
     # Step 3: create buffer in trainer
--- gops/sys_simulator/run_lqs3a1_infadp.py
+++ gops/sys_simulator/run_lqs3a1_infadp.py
@@ -2,11 +2,11 @@ from sys_run import PolicyRuner
 
 runer = PolicyRuner(
     log_policy_dir_list=["../../results/INFADP/s3a1"]*2,
-    trained_policy_iteration_list=['9000','10000'],
+    trained_policy_iteration_list=['19500','20000'],
     is_init_info=True,
-    init_info={"init_state":[0.5, 0.5,0.5]},
+    init_info={"init_state":[0.5, 0.1,0.1]},
     save_render=False,
-    legend_list=['9000','9500'],
+    legend_list=['19500','20000'],
     dt=0.1,
     use_opt=True)
 
