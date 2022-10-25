3ffc6f427db3c72c63d0bdab9990dbe09e0d9b7c
yangyujie <yangyj21@mails.tsinghua.edu.cn>
2022-10-25 19:48:29 +0800

    rewrite lq model use the base model

--- gops/env/env_ocp/resources/lq_base.py
+++ gops/env/env_ocp/resources/lq_base.py
@@ -1,6 +1,6 @@
 import math
 import warnings
-from typing import Union
+from typing import Tuple, Union
 
 import gym
 import matplotlib.pyplot as plt
@@ -10,6 +10,7 @@ from gym.spaces import Box
 from scipy.linalg._solvers import solve_discrete_are
 
 from gops.env.env_ocp.pyth_base_data import PythBaseEnv
+from gops.env.env_ocp.pyth_base_model import PythBaseModel
 
 warnings.filterwarnings("ignore")
 gym.logger.setLevel(gym.logger.ERROR)
@@ -34,19 +35,17 @@ class LQDynamics:
             IA = IA.numpy()
         self.inv_IA = torch.as_tensor(np.linalg.pinv(IA), dtype=torch.float32)
 
-
     def compute_control_matrix(self):
         gamma = 0.99
         A0 = self.A.numpy().astype('float64')
-        A = np.linalg.pinv(np.eye(A0.shape[0])-A0*self.time_step)*np.sqrt(gamma)
-        B0=self.B.numpy().astype('float64')
-        B = A@B0*self.time_step
-        Q= np.diag(self.Q.numpy()).astype('float64')
-        R=np.diag(self.R.numpy()).astype('float64')
-        P = solve_discrete_are(A,B,Q,R)
-        K = np.linalg.pinv(R+B.T@P@B)@B.T@P@A
+        A = np.linalg.pinv(np.eye(A0.shape[0]) - A0 * self.time_step) * np.sqrt(gamma)
+        B0 = self.B.numpy().astype('float64')
+        B = A @ B0 * self.time_step
+        Q = np.diag(self.Q.numpy()).astype('float64')
+        R = np.diag(self.R.numpy()).astype('float64')
+        P = solve_discrete_are(A, B, Q, R)
+        K = np.linalg.pinv(R + B.T @ P @ B) @ B.T @ P @ A
         return K
-
 
     def f_xu_old(self, x: torch.Tensor, u: torch.Tensor) -> torch.Tensor:
         """
@@ -95,7 +94,7 @@ class LQDynamics:
 
     def prediction(self,
                    x_t: Union[torch.Tensor, np.ndarray], u_t: Union[torch.Tensor, np.ndarray]
-    ) -> Union[torch.Tensor, np.ndarray]:
+                   ) -> Union[torch.Tensor, np.ndarray]:
         numpy_flag = False
         if isinstance(x_t, np.ndarray):
             x_t = torch.as_tensor(x_t, dtype=torch.float32).unsqueeze(0)
@@ -132,7 +131,7 @@ class LQDynamics:
             numpy_flag = True
         reward_state = torch.sum(torch.pow(x_t, 2) * self.Q, dim=-1)
         reward_action = torch.sum(torch.pow(u_t, 2) * self.R, dim=-1)
-        reward = self.reward_scale * (self.reward_shift - 1.0 * (reward_state + reward_action) )
+        reward = self.reward_scale * (self.reward_shift - 1.0 * (reward_state + reward_action))
         if numpy_flag:
             reward = reward[0].item()
         return reward
@@ -186,8 +185,8 @@ class LqEnv(PythBaseEnv):
     def has_optimal_controller(self):
         return True
 
-    def control_policy(self,state):
-        return -self.control_matrix@state
+    def control_policy(self, state):
+        return -self.control_matrix @ state
 
     def reset(self, init_state=None, **kwargs):
         self.step_counter = 0
@@ -301,9 +300,8 @@ class LqEnv(PythBaseEnv):
         plt.clf()
 
 
-class LqModel(torch.nn.Module):
+class LqModel(PythBaseModel):
     def __init__(self, config, **kwargs):
-        super().__init__()
         """
         you need to define parameters here
         """
@@ -318,77 +316,20 @@ class LqModel(torch.nn.Module):
         hb_action = np.array(config["action_high"])
         self.dt = config["dt"]  # seconds between state updates
 
-        # do not change the following section
-        self.lb_state = torch.tensor(lb_state, dtype=torch.float32)
-        self.hb_state = torch.tensor(hb_state, dtype=torch.float32)
-        self.lb_action = torch.tensor(lb_action, dtype=torch.float32)
-        self.hb_action = torch.tensor(hb_action, dtype=torch.float32)
+        super().__init__(
+            obs_lower_bound=lb_state,
+            obs_upper_bound=hb_state,
+            action_lower_bound=lb_action,
+            action_upper_bound=hb_action,
+        )
 
-    def forward(self, state: torch.Tensor, action: torch.Tensor, info, beyond_done=None):
-        """
-        rollout the model one step, notice this method will not change the value of self.state
-        you need to define your own state transition  function here
-        notice that all the variables contains the batch dim you need to remember this point
-        when constructing your function
-        :param state: datatype:torch.Tensor, shape:[batch_size, state_dim]
-        :param action: datatype:torch.Tensor, shape:[batch_size, action_dim]
-        :param beyond_done: flag indicate the state is already done which means it will not be calculated by the model
-        :return:
-                next_state:  datatype:torch.Tensor, shape:[batch_size, state_dim]
-                              the state will not change anymore when the corresponding flag done is set to True
-                reward:  datatype:torch.Tensor, shape:[batch_size,]
-                isdone:   datatype:torch.Tensor, shape:[batch_size,]
-                         flag done will be set to true when the model reaches the max_iteration or the next state
-                         satisfies ending condition
-
-                info: datatype: dict, any useful information for debug or training, including constraint
-                        {"constraint": None}
-        """
-        warning_msg = "action out of action space!"
-        if not ((action <= self.hb_action).all() and (action >= self.lb_action).all()):
-            warnings.warn(warning_msg)
-            action = clip_by_tensor(action, self.lb_action, self.hb_action)
-
-        warning_msg = "state out of state space!"
-        if not ((state <= self.hb_state).all() and (state >= self.lb_state).all()):
-            warnings.warn(warning_msg)
-            state = clip_by_tensor(state, self.lb_state, self.hb_state)
-
-        #  define your forward function here: the format is just like: state_next = f(state,action)
-        state_next = self.dynamics.prediction(state, action)
-
-        ############################################################################################
-
-        # define the ending condation here the format is just like isdone = l(next_state)
-        isdone = torch.full([state.size()[0]], False, dtype=torch.bool)
-
-        ############################################################################################
-
-        # define the reward function here the format is just like: reward = l(state,state_next,reward)
-        reward = self.dynamics.compute_reward(state, action).reshape(-1)
-
-        ############################################################################################
-        if beyond_done is None:
-            beyond_done = torch.full([state.size()[0]], False, dtype=torch.float32)
-
-        beyond_done = beyond_done.bool()
-        mask = isdone | beyond_done
-        mask = torch.unsqueeze(mask, -1)
-        state_next = ~mask * state_next + mask * state
-        reward = ~(beyond_done) * reward
-        return state_next, reward, mask.squeeze(), {"constraint": None}
-
-def clip_by_tensor(t, t_min, t_max):
-    """
-    clip_by_tensor
-    :param t: tensor
-    :param t_min: min
-    :param t_max: max
-    :return: cliped tensor
-    """
-    result = (t >= t_min) * t + (t < t_min) * t_min
-    result = (result <= t_max) * result + (result > t_max) * t_max
-    return result
+    def step(self, obs: torch.Tensor, action: torch.Tensor, info: dict) \
+            -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, dict]:
+        next_obs = self.dynamics.prediction(obs, action)
+        reward = self.dynamics.compute_reward(obs, action).reshape(-1)
+        done = torch.full([obs.size()[0]], False, dtype=torch.bool)
+        info = {"constraint": None}
+        return next_obs, reward, done, info
 
 
 def test_check():
@@ -408,7 +349,7 @@ def test_env():
     env.reset()
     print(env.has_optimal_controller)
     print(env.control_matrix)
-    a0 = env.control_policy(np.array([0.1,0.2,-0.3,0,0]))
+    a0 = env.control_policy(np.array([0.1, 0.2, -0.3, 0, 0]))
     print(a0)
     for _ in range(100):
         a = env.action_space.sample()
@@ -420,4 +361,3 @@ if __name__ == "__main__":
     # test_dynamic()
     # test_check()
     test_env()
-
