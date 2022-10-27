5d99aa9cebe5b9f9a245d96c88b02820f216cd7b
zzl <zhengzlthu@163.com>
2022-10-27 15:58:46 +0800

    modify the way to get constraint and terminal cost modify the way to call model.forward adapt to the base model rm redundant codes

--- gops/sys_simulator/sys_opt_controller.py
+++ gops/sys_simulator/sys_opt_controller.py
@@ -1,6 +1,5 @@
 import argparse
 from typing import List, Tuple
-import control as ct
 from gops.create_pkg.create_env import create_env
 from gops.create_pkg.create_env_model import create_env_model
 from gops.env.env_ocp.pyth_base_model import PythBaseModel
@@ -24,12 +23,11 @@ class OptController:
         self.model = model
         self.ctrl_dt = ctrl_dt
         self.sim_dt = model.dt
-        self.state_dim = model.state_dim
+        self.obs_dim = model.obs_dim
         self.action_dim = model.action_dim
         self.num_pred_step = num_pred_step
         self.initial_guess = np.zeros(self.action_dim * num_pred_step)
         self.minimize_options = minimize_options
-
         self.bounds = opt.Bounds(
             np.tile(self.model.action_lower_bound, (num_pred_step,)), 
             np.tile(self.model.action_upper_bound, (num_pred_step,))
@@ -42,16 +40,13 @@ class OptController:
             x0=self.initial_guess,
             args=(x,),
             jac=self.__cost_jac,
-            # method="SLSQP",
-            # bounds=self.bounds,
-            # bounds=[(self.model.action_lower_bound, self.model.action_upper_bound)] * num_pred_step,
             bounds=opt._constraints.new_bounds_to_old(self.bounds.lb, self.bounds.ub, num_pred_step * self.action_dim),
-            # constraints=[{
-            #     "type": "ineq", 
-            #     "fun": self.__constraint_fcn,
-            #     "jac": self.__constraint_jac,
-            #     "args": (x,)
-            # }],
+            constraints=[{
+                "type": "ineq", 
+                "fun": self.__constraint_fcn,
+                "jac": self.__constraint_jac,
+                "args": (x,)
+            }],
             options=self.minimize_options
         )
         self.initial_guess = np.concatenate((res.x[self.action_dim:], np.zeros(self.action_dim)))
@@ -59,62 +54,41 @@ class OptController:
         return res.x.reshape((self.action_dim, -1))[:, 0]
 
     def __cost_fcn(self, inputs: np.ndarray, x: np.ndarray) -> float:
-
         x = torch.tensor(x, dtype=torch.float32)
         inputs = torch.tensor(
             inputs, 
             dtype=torch.float32, 
             requires_grad=True
         )
-        states, rewards = self.__rollout(inputs, x)
-
-        # sum up the intergral costs from timestep 0 to T-1
-        cost = torch.sum(rewards)
-
-        # Terminal cost for timestep T
-        terminal_cost = self.model.get_terminal_cost(states[:, -1])
-        cost += terminal_cost if terminal_cost is not None else torch.tensor(0)
-        
+        cost = self.__compute_cost(inputs, x)
         return cost.detach().item()
 
     def __cost_jac(self, inputs: np.ndarray, x: np.ndarray) -> np.ndarray:
-
         x = torch.tensor(x, dtype=torch.float32)
         inputs = torch.tensor(
             inputs, 
             dtype=torch.float32, 
             requires_grad=True
         )
-        states, rewards = self.__rollout(inputs, x)
-
-        # sum up the intergral costs from timestep 0 to T-1
-        cost = torch.sum(rewards)
-
-        # Terminal cost for timestep T
-        terminal_cost = self.model.get_terminal_cost(states[:, -1])
-        cost += terminal_cost if terminal_cost is not None else torch.tensor(0)
+        cost = self.__compute_cost(inputs, x)
         jac = torch.autograd.grad(cost, inputs)[0]
         return jac.numpy().astype("d")
 
     def __constraint_fcn(self, inputs: np.ndarray, x: np.ndarray) -> torch.Tensor:
-        self.constraint_evaluations += 1
 
-        x = torch.tensor(x, dtype=torch.float32)
-        inputs = torch.tensor(
-            inputs, 
-            dtype=torch.float32
-        )
-        states, _ = self.__rollout(inputs, x)
-
-
-        constraints = self.model.get_constraint(states)
-        if constraints is None:
+        if self.model.get_constraint is None:
             return torch.tensor([0.])
-        # constraints = torch.cat((
-        #     states.reshape((-1)) - torch.tile(self.model.lb_state, (num_pred_step + 1,)), 
-        #     torch.tile(self.model.hb_state, (num_pred_step + 1,)) - states.reshape((-1))
-        # ))
-        return constraints
+        else:
+            self.constraint_evaluations += 1
+
+            x = torch.tensor(x, dtype=torch.float32)
+            inputs = torch.tensor(
+                inputs, 
+                dtype=torch.float32
+            )
+            states, _ = self.__rollout(inputs, x)
+            
+            return self.model.get_constraint(states)
 
     def __constraint_jac(self, inputs: np.ndarray, x: np.ndarray) -> np.ndarray:
         x = torch.tensor(x, dtype=torch.float32)
@@ -129,24 +103,37 @@ class OptController:
     def __rollout(self, inputs: torch.Tensor, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, List]:
         self.system_simulations += 1
         inputs_reshaped = inputs.reshape((self.action_dim , -1))
-        states = torch.zeros((self.state_dim, self.num_pred_step + 1))
+        states = torch.zeros((self.obs_dim, self.num_pred_step + 1))
         rewards = torch.zeros(self.num_pred_step)
         states[:, 0] = x
+        done = torch.tensor([False])
+        info = {}
 
-        # rollout the states and rewards
         for i in range(self.num_pred_step):
             next_x = states[:, i].clone().unsqueeze(0)
             u = inputs_reshaped[:, i].unsqueeze(0)
             for _ in range(int(self.ctrl_dt / self.sim_dt)):
-                next_x, reward, _, _ = self.model.forward(
+                next_x, reward, done, info = self.model.forward(
                     next_x, 
                     u,
-                    done=False,
-                    info={}
+                    done=done,
+                    info=info
                 )
             states[:, i + 1] = next_x
             rewards[i] = -reward
         return states, rewards
+    
+    def __compute_cost(self, inputs: torch.Tensor, x: torch.Tensor) -> torch.Tensor:
+        # rollout the states and rewards
+        states, rewards = self.__rollout(inputs, x)
+
+        # sum up the intergral costs from timestep 0 to T-1
+        cost = torch.sum(rewards)
+
+        # Terminal cost for timestep T
+        if self.model.get_terminal_cost is not None:
+            cost += self.model.get_terminal_cost(states[:, -1])
+        return cost
 
     def __reset_statistics(self):
         """Reset counters for keeping track of statistics"""
@@ -195,6 +182,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--env_id", type=str, default="pyth_lq")
     parser.add_argument("--lq_config", type=str, default="s4a2")
+    parser.add_argument('--clip_action', type=bool, default=True)
+    parser.add_argument('--clip_obs', type=bool, default=True)
+    parser.add_argument('--mask_at_done', type=bool, default=True)
     # parser.add_argument(
     #     "--is_adversary", type=bool, default=False, help="Adversary training"
     # )
@@ -208,7 +198,7 @@ if __name__ == "__main__":
 
     args = vars(parser.parse_args())
     env_model = create_env_model(**args)
-    state_dim = env_model.state_dim
+    obs_dim = env_model.obs_dim
     action_dim = env_model.action_dim
 
     ctrl_dt = env_model.dt
@@ -217,36 +207,20 @@ if __name__ == "__main__":
         mean_state_errs = []
         max_action_errs = []
         mean_action_errs = []
+        K = env_model.dynamics.compute_control_matrix()
     
-    # num_pred_steps = range(50, 60, 20)
-    num_pred_steps = (70,)
+    num_pred_steps = range(15, 40, 15)
+    # num_pred_steps = (30,)
     for num_pred_step in num_pred_steps:
-        if args["env_id"] == "pyth_lq":
-            A_conti = env_model.dynamics.A.numpy()
-            B_conti = env_model.dynamics.B.numpy()
-            Q = np.diag(env_model.dynamics.Q.numpy())
-            R = np.diag(env_model.dynamics.R.numpy())
-            IA = np.eye(env_model.state_dim) - A_conti * env_model.dt
-            inv_IA = np.linalg.pinv(IA)
-            A = inv_IA
-            B = inv_IA @ B_conti * env_model.dt
-            K, S, _ = ct.dlqr(A, B, Q, R)
-            S = torch.tensor(S, dtype=torch.float32)
-            
-            def terminal_cost(x: torch.Tensor):
-                return x.T @ S @ x
-
         controller = OptController(
             env_model, 
             ctrl_dt=ctrl_dt, 
             num_pred_step=num_pred_step, 
             minimize_options={
-                "max_iter": 200, 
+                "max_iter": 500, 
                 "tol": 1e-3,
                 "acceptable_tol": 1e-1,
-                # "ftol": 1e-8,
-                # "disp": True,
-                # "iprint": 2
+                "acceptable_iter": 10,
                 "print_level": 5,
             }
         )
@@ -297,8 +271,8 @@ if __name__ == "__main__":
 
     #=======state-timestep=======#
     plt.figure()
-    for i in range(state_dim):
-        plt.subplot(state_dim, 1, i + 1)
+    for i in range(obs_dim):
+        plt.subplot(obs_dim, 1, i + 1)
         plt.plot(sim_horizon, xs[:, i], label="mpc")
         if args["env_id"] == "pyth_lq":
             plt.plot(sim_horizon, xs_lqr[:, i], label="lqr")
@@ -330,11 +304,12 @@ if __name__ == "__main__":
     #=======error-predstep=======#
     if args["env_id"] == "pyth_lq":
         plt.figure()
-        for i in range(state_dim):
+        for i in range(obs_dim):
             plt.plot(num_pred_steps, max_state_errs[:, i], label=f"State-{i+1}")
         plt.legend()
         plt.xlabel("num pred step")
         plt.ylabel(f"State Max error (%)")
+        plt.yscale ('log')
         plt.savefig(f"State-max-err-{args['env_id']}-{args['lq_config']}.png")
 
         plt.figure()
@@ -343,14 +318,16 @@ if __name__ == "__main__":
         plt.legend()
         plt.xlabel("num pred step")
         plt.ylabel(f"Action Max error (%)")
+        plt.yscale ('log')
         plt.savefig(f"Action-max-err-{args['env_id']}-{args['lq_config']}.png")
 
         plt.figure()
-        for i in range(state_dim):
+        for i in range(obs_dim):
             plt.plot(num_pred_steps, mean_state_errs[:, i], label=f"State-{i+1}")
         plt.legend()
         plt.xlabel("num pred step")
         plt.ylabel(f"State Mean error (%)")
+        plt.yscale ('log')
         plt.savefig(f"State-mean-err-{args['env_id']}-{args['lq_config']}.png")
 
         plt.figure()
@@ -359,4 +336,5 @@ if __name__ == "__main__":
         plt.legend()
         plt.xlabel("num pred step")
         plt.ylabel(f"Action Mean error (%)")
+        plt.yscale ('log')
         plt.savefig(f"Action-mean-err-{args['env_id']}-{args['lq_config']}.png")
