96abb032c9a6083afc7eb174cfc1d93b15f0d363
songxj_idlab_ubuntu <songxj21@mails.tsinghua.edu.cn>
2022-11-08 22:03:38 +0800

    Update env_dynamic_checker.py

--- gops/env/tools/env_dynamic_checker.py
+++ gops/env/tools/env_dynamic_checker.py
@@ -10,13 +10,16 @@
 import numpy as np
 import warnings
 import os
+import copy
+import json
+import datetime
 import argparse
 import torch
 from itertools import cycle
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
-from gops.utils.common_utils import get_args_from_json
+from gops.utils.common_utils import change_type, get_args_from_json
 from gops.utils.plot_evaluation import cm2inch
 from gops.create_pkg.create_env import create_env
 
@@ -130,7 +133,7 @@ def draw_figures(traj_data, df_data, ddf_data, ddf_df_data, state_index, close_l
         fig.savefig(os.path.join(save_path, 'Open-loop Dynamic Check of State-%d.png'%state_index))
     
 
-def check_dynamic(env=None, traj_num=5, init_info=None, log_policy_dir=None, policy_iteration=None, save_path=None):
+def check_dynamic(env=None, traj_num=5, init_info=None, log_policy_dir=None, policy_iteration=None):
     """
     check whether the dynamic characteristic is well behaved.
 
@@ -152,6 +155,27 @@ def check_dynamic(env=None, traj_num=5, init_info=None, log_policy_dir=None, pol
     
     assert env is not None
 
+    save_path = os.path.join('./', 'figures')
+    if hasattr(env, 'config_name'):
+        env_name = '%s_%s'%(env.env_id, env.config_name)
+    else:
+        env_name = env.env_id
+    
+    if close_loop:
+        save_path = os.path.join(save_path, '%s_close_test'%env_name)
+    else:
+        save_path = os.path.join(save_path, '%s_open_test'%env_name)
+    save_path = os.path.join(save_path, datetime.datetime.now().strftime("%y%m%d-%H%M%S"))
+    os.makedirs(save_path, exist_ok=True)
+
+    args = {'env_name':env_name,
+            'traj_num':traj_num,
+            'init_info':init_info,
+            'log_policy_dir':log_policy_dir,
+            'policy_iteration':policy_iteration}
+    with open(os.path.join(save_path, "config.json"), "w", encoding="utf-8") as f:
+        json.dump(change_type(copy.deepcopy(args)), f, ensure_ascii=False, indent=4)
+
     if init_info is not None:
         assert len(init_info["init_state"]) == traj_num
 
@@ -160,8 +184,15 @@ def check_dynamic(env=None, traj_num=5, init_info=None, log_policy_dir=None, pol
     df_state_list = []
     ddf_state_list = []
 
-    stable_final = np.ones(env.work_space.shape[1]).astype(np.bool_)
-    stable_final_threshold = (env.work_space[1] - env.work_space[0]) * 0.0001
+    if hasattr(env, 'work_space'):
+        state_dim = env.work_space.shape[1]
+        state_range = env.work_space[1] - env.work_space[0]
+    else:
+        state_dim = env.observation_space.shape[0]
+        state_range = 1 # the range is usually [-inf, inf] for gym, so just give 1 here for simplicity
+    
+    stable_final = np.ones(state_dim).astype(np.bool_)
+    stable_final_threshold = state_range * 0.0001
 
     for episode in range(traj_num):
         if init_info is not None:
@@ -177,7 +208,7 @@ def check_dynamic(env=None, traj_num=5, init_info=None, log_policy_dir=None, pol
         done = False
 
         for step_index in range(20010):
-            state_list[-1].append((state, step_index))
+            state_list[-1].append((obs, step_index))
 
             if close_loop:
                 action = compute_action(obs, controller)
@@ -195,24 +226,24 @@ def check_dynamic(env=None, traj_num=5, init_info=None, log_policy_dir=None, pol
                 
             state_next = env.state
 
-            df_state_list[-1].append((state_next - state, state))
+            df_state_list[-1].append((obs_next - obs, obs))
 
             if step_index > 0:
-                ddf_state_list[-1].append((df_state_list[-1][-1][0] - df_state_list[-1][-2][0], df_state_list[-1][-1][0], state))
+                ddf_state_list[-1].append((df_state_list[-1][-1][0] - df_state_list[-1][-2][0], df_state_list[-1][-1][0], obs))
 
             state = state_next
             obs = obs_next
 
     # Second, raise warning if dynamic change too fast
-    df_threshold = (env.work_space[1] - env.work_space[0]) * 0.4
-    df_too_fast = np.zeros(env.work_space.shape[1]).astype(np.bool_)
+    df_threshold = state_range * 0.4
+    df_too_fast = np.zeros(state_dim).astype(np.bool_)
     for k in range(len(df_state_list)):
         for i in range(len(df_state_list[k])):
             df_state, _ = df_state_list[k][i]
             df_too_fast = df_too_fast | (df_state > df_threshold)
         
-    ddf_threshold = (env.work_space[1] - env.work_space[0]) * 0.3
-    ddf_too_fast = np.zeros(env.work_space.shape[1]).astype(np.bool_)
+    ddf_threshold = state_range * 0.3
+    ddf_too_fast = np.zeros(state_dim).astype(np.bool_)
     for k in range(len(df_state_list)):
         for i in range(len(ddf_state_list[k])):
             ddf_state, _, _ = ddf_state_list[k][i]
@@ -221,12 +252,12 @@ def check_dynamic(env=None, traj_num=5, init_info=None, log_policy_dir=None, pol
     warnings.filterwarnings("always")
     origin_format = warnings.formatwarning
     warnings.formatwarning = warning_on_one_line
-    for i in range(env.work_space.shape[1]):
+    for i in range(state_dim):
         if df_too_fast[i] or ddf_too_fast[i]:
             warnings.warn("the %d-th state changes too fast, please check the environment dynamic. If you ensure there is no problems, please ignore the warning." % i)
     '''
     # Third, raise warning if the open-loop system not stabilizes beyond time limit
-    for i in range(env.work_space.shape[1]):
+    for i in range(state_dim):
         if not stable_final[i]:
             warnings.warn("the %d-th state can not stablize without action, please check the environment. If you ensure there is no problems, please ignore the warning." % i)
     '''
@@ -234,7 +265,7 @@ def check_dynamic(env=None, traj_num=5, init_info=None, log_policy_dir=None, pol
     warnings.filterwarnings("ignore")
 
     # Fourth, draw df_state and ddf_state distribution figures with trajectory figures
-    for i in range(env.work_space.shape[1]):
+    for i in range(state_dim):
         traj_data = [dict({"x":[x for y, x in state_list[k]],\
                           "y":[y[i] for y, x in state_list[k]]})
                      for k in range(traj_num)]
@@ -279,18 +310,21 @@ if __name__ == "__main__":
 
     (7.a) open-loop check of pyth_veh3dofconti
     (7.b) close-loop check of pyth_veh3dofconti
+
+    (8.a) open-loop check of gym_pendulum
+    (8.b) close-loop check of gym_pendulum
     '''
 
     '''(1.a) open-loop check of pyth_lqs2a1_data'''
-    # env = create_env(env_id='pyth_lq', lq_config='s2a1')
-    # check_dynamic(env, traj_num=2,
-    #               init_info={"init_state": [[0.5, -0.5], [1, -1]]})
+    env = create_env(env_id='pyth_lq', lq_config='s2a1')
+    check_dynamic(env, traj_num=2,
+                  init_info={"init_state": [[0.5, -0.5], [1, -1]]})
     
     '''(1.b) close-loop check of pyth_lqs2a1_data'''
-    env = create_env(env_id='pyth_lq', lq_config='s2a1')
-    check_dynamic(env,
-                  log_policy_dir='./results/INFADP/s2a1',
-                  policy_iteration='350000')
+    # env = create_env(env_id='pyth_lq', lq_config='s2a1')
+    # check_dynamic(env,
+    #               log_policy_dir='./results/INFADP/s2a1',
+    #               policy_iteration='350000')
 
     '''(2.a) open-loop check of pyth_lqs4a2_data'''
     # env = create_env(env_id='pyth_lq', lq_config='s4a2')
@@ -354,3 +388,13 @@ if __name__ == "__main__":
     # check_dynamic(env, traj_num=1, init_info={"init_state": [[0., 0., 0., 10., 0, 0]], "ref_time": [0.], "ref_num": [0.]},
     #               log_policy_dir='./results/INFADP/veh3dofconti_221017-210557',
     #               policy_iteration='4000')
+
+    '''(8.a) open-loop check of gym_pendulum'''
+    # env = create_env(env_id='gym_pendulum')
+    # check_dynamic(env, traj_num=2)
+
+    '''(8.b) close-loop check of gym_pendulum'''
+    # env = create_env(env_id='gym_pendulum')
+    # check_dynamic(env, traj_num=2,
+    #               log_policy_dir='./results/DDPG/gym_pendulum',
+    #               policy_iteration='8000')
