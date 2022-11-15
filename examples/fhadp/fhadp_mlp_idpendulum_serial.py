f9361a56298547200408b850874bd87b0f753091
wenxuanwang <17888819590@163.com>
2022-11-15 22:36:31 +0800

    1. modify fhadp.py 2. add example

--- examples/fhadp/fhadp_mlp_idpendulum_serial.py
+++ examples/fhadp/fhadp_mlp_idpendulum_serial.py
@@ -8,8 +8,7 @@
 
 import os
 
-os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
-os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "4"
 import argparse
 import numpy as np
 from gops.create_pkg.create_alg import create_alg
@@ -21,7 +20,6 @@ from gops.create_pkg.create_trainer import create_trainer
 from gops.utils.init_args import init_args
 from gops.utils.plot_evaluation import plot_all
 from gops.utils.tensorboard_setup import start_tensorboard, save_tb_to_csv
-from gops.utils.common_utils import seed_everything
 
 if __name__ == "__main__":
     # Parameters Setup
@@ -31,22 +29,24 @@ if __name__ == "__main__":
     # Key Parameters for users
     parser.add_argument("--env_id", type=str, default="pyth_idpendulum")
     parser.add_argument("--algorithm", type=str, default="FHADP")
-    parser.add_argument("--pre_horizon", type=int, default=30)
+    parser.add_argument("--pre_horizon", type=int, default=350)
     parser.add_argument("--enable_cuda", default=False, help="Enable CUDA")
     ################################################
     # 1. Parameters for environment
-    parser.add_argument("--obsv_dim", type=int, default=4, help="dim(State)")
-    parser.add_argument("--action_dim", type=int, default=1, help="dim(Action)")
+    parser.add_argument("--obsv_dim", type=int, default=None)
+    parser.add_argument("--action_dim", type=int, default=None)
     parser.add_argument("--action_high_limit", type=list, default=None)
     parser.add_argument("--action_low_limit", type=list, default=None)
-    parser.add_argument(
-        "--action_type", type=str, default="continu", help="Options: continu/discret"
-    )
-    parser.add_argument(
-        "--is_render", type=bool, default=False, help="Draw environment animation"
-    )
+    parser.add_argument("--action_type", type=str, default="continu")
+    parser.add_argument("--reward_scale", type=float, default=0.1)
+    parser.add_argument("--repeat_num", type=int, default=None)
+    parser.add_argument("--sum_reward", type=bool, default=False)
+    parser.add_argument("--is_render", type=bool, default=False)
     parser.add_argument(
         "--is_adversary", type=bool, default=False, help="Adversary training"
+    )
+    parser.add_argument(
+        "--is_constrained", type=bool, default=False, help="Adversary training"
     )
     ################################################
     # 2.1 Parameters of value approximate function
@@ -60,17 +60,17 @@ if __name__ == "__main__":
     policy_func_type = parser.parse_known_args()[0].policy_func_type
     if policy_func_type == "MLP":
         parser.add_argument("--policy_hidden_sizes", type=list, default=[256, 256])
-        parser.add_argument("--policy_hidden_activation", type=str, default="elu")
+        parser.add_argument("--policy_hidden_activation", type=str, default="gelu")
         parser.add_argument("--policy_output_activation", type=str, default="linear")
 
     ################################################
     # 3. Parameters for RL algorithm
-    parser.add_argument("--policy_learning_rate", type=float, default=3e-5)
+    parser.add_argument("--policy_learning_rate", type=float, default=3e-4)
 
     ################################################
     # 4. Parameters for trainer
     parser.add_argument("--trainer", type=str, default="off_serial_trainer")
-    parser.add_argument("--max_iteration", type=int, default=2000)
+    parser.add_argument("--max_iteration", type=int, default=50000)
     trainer_type = parser.parse_known_args()[0].trainer
     parser.add_argument("--ini_network_dir", type=str, default=None)
     if trainer_type == "off_serial_trainer":
@@ -82,26 +82,26 @@ if __name__ == "__main__":
     ################################################
     # 5. Parameters for sampler
     parser.add_argument("--sampler_name", type=str, default="off_sampler")
-    parser.add_argument("--sample_batch_size", type=int, default=256)
+    parser.add_argument("--sample_batch_size", type=int, default=1)
     parser.add_argument(
         "--noise_params",
         type=dict,
         default={
             "mean": np.array([0], dtype=np.float32),
-            "std": np.array([0.2], dtype=np.float32),
+            "std": np.array([0.0], dtype=np.float32),
         },
     )
 
     ################################################
     # 7. Parameters for evaluator
     parser.add_argument("--evaluator_name", type=str, default="evaluator")
-    parser.add_argument("--num_eval_episode", type=int, default=100)
+    parser.add_argument("--num_eval_episode", type=int, default=10)
     parser.add_argument("--eval_interval", type=int, default=100)
 
     ################################################
     # 8. Data savings
     parser.add_argument("--save_folder", type=str, default=None)
-    parser.add_argument("--apprfunc_save_interval", type=int, default=100)
+    parser.add_argument("--apprfunc_save_interval", type=int, default=2000)
     parser.add_argument("--log_save_interval", type=int, default=100)
 
     # Get parameter dictionary
@@ -111,9 +111,6 @@ if __name__ == "__main__":
     start_tensorboard(args["save_folder"])
     # Step 1: create algorithm and approximate function
     alg = create_alg(**args)
-    alg.set_parameters(
-        {"reward_scale": 0.1, "gamma": 0.99, "tau": 0.2, "delay_update": 1}
-    )
     # Step 2: create sampler in trainer
     sampler = create_sampler(**args)
     # Step 3: create buffer in trainer
