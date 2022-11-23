f8fd574965d5c45da35d75a6aaa03fb397b2d01f
yangyujie <yangyj21@mails.tsinghua.edu.cn>
2022-11-23 16:34:20 +0800

    update veh2dof errcstr env

--- gops/env/env_ocp/pyth_veh2dofconti_errcstr_data.py
+++ gops/env/env_ocp/pyth_veh2dofconti_errcstr_data.py
@@ -5,6 +5,8 @@
 #  Creator: iDLab
 #  Description: Vehicle 2DOF data environment with tracking error constraint
 
+from typing import Dict, Optional
+
 import numpy as np
 
 from gops.env.env_ocp.pyth_veh2dofconti_data import SimuVeh2dofconti
@@ -13,12 +15,13 @@ from gops.env.env_ocp.pyth_veh2dofconti_data import SimuVeh2dofconti
 class SimuVeh2dofcontiErrCstr(SimuVeh2dofconti):
     def __init__(
         self,
-        path_para:dict = None, 
-        u_para:dict = None, 
+        pre_horizon: int = 10,
+        path_para: Optional[Dict[str, Dict]] = None,
+        u_para: Optional[Dict[str, Dict]] = None,
         y_error_tol: float = 0.2, 
         **kwargs,
     ):
-        super().__init__(path_para, u_para, **kwargs)
+        super().__init__(pre_horizon, path_para, u_para, **kwargs)
         self.y_error_tol = y_error_tol
         self.info_dict.update({
             "constraint": {"shape": (1,), "dtype": np.float32},
@@ -26,7 +29,7 @@ class SimuVeh2dofcontiErrCstr(SimuVeh2dofconti):
 
     def get_constraint(self) -> np.ndarray:
         y = self.state[0]
-        y_ref = self.ref_traj.compute_y(self.t, self.path_num, self.u_num)
+        y_ref = self.ref_points[0, 0]
         constraint = np.array([abs(y - y_ref) - self.y_error_tol], dtype=np.float32)
         return constraint
 
