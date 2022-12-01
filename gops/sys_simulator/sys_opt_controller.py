6906652532b02eb489f63c51e114d4d5e748a6a4
zzl <zhengzlthu@163.com>
2022-12-01 22:45:50 +0800

    delete NNController

--- gops/sys_simulator/sys_opt_controller.py
+++ gops/sys_simulator/sys_opt_controller.py
@@ -255,29 +255,6 @@ class OptController:
             self.__reset_statistics()
 
 
-class NNController:
-    def __init__(self, args, log_policy_dir):
-        print(args)
-        alg_name = args["algorithm"]
-        alg_file_name = alg_name.lower()
-        file = __import__(alg_file_name)
-        ApproxContainer = getattr(file, "ApproxContainer")
-        self.networks = ApproxContainer(**args)
-
-        for filename in os.listdir(log_policy_dir + "/apprfunc"):
-            if filename.endswith("_opt.pkl"):
-                log_path = os.path.join(log_policy_dir, "apprfunc", filename)
-        self.networks.load_state_dict(torch.load(log_path))
-
-    def __call__(self, obs):
-        batch_obs = torch.from_numpy(np.expand_dims(obs, axis=0).astype("float32"))
-        logits = self.networks.policy(batch_obs)
-        action_distribution = self.networks.create_action_distributions(logits)
-        action = action_distribution.mode()
-        action = action.detach().numpy()[0]
-        return action
-
-
 if __name__ == "__main__":
     # Parameters Setup
     parser = argparse.ArgumentParser()
