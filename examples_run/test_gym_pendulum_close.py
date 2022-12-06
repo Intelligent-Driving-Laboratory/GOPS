76424c6d7bc49dca4188bd0230705c7e1ebd1c5f
jerry99 <songxj21@mails.tsinghua.edu.cn>
2022-12-06 11:38:49 +0800

    add comments in test_*_*.py

--- examples_run/test_gym_pendulum_close.py
+++ examples_run/test_gym_pendulum_close.py
@@ -1,4 +1,15 @@
-from gops.create_pkg.create_env import create_env
+#  Copyright (c). All Rights Reserved.
+#  General Optimal control Problem Solver (GOPS)
+#  Intelligent Driving Lab (iDLab), Tsinghua University
+#
+#  Creator: iDLab
+#  Lab Leader: Prof. Shengbo Eben Li
+#  Email: lisb04@gmail.com
+#
+#  Description: check the close-loop dynamic of gym_pendulum, draw the figures of first/second-order difference of state.
+#               figures will be saved in 'figures' folder.
+#  Update: 2022-12-05, Xujie Song: create file
+
 from gops.env.tools.env_dynamic_checker import check_dynamic
 
 
