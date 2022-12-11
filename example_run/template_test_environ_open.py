0ad5d13acf4e34209dabd5c408977eec48a38d47
jerry99 <songxj21@mails.tsinghua.edu.cn>
2022-12-11 18:49:50 +0800

    update test template

--- example_run/template_test_environ_open.py
+++ example_run/template_test_environ_open.py
@@ -7,23 +7,23 @@
 #  Email: lisb04@gmail.com
 #
 #  Description: Template for checking open-loop dynamics whether its time-discretizaiton is reasonable!
-#  Update: 2020-12-10, Xujie Song: create example template
-
-import argparse
+#  Update: 2022-12-10, Xujie Song: create example template
 
 from gops.env.inspector.env_dynamic_checker import check_dynamic
 
-if __name__ == "__main__":
-    # Parameters Setup
-    parser = argparse.ArgumentParser()
+check_dynamic(
+    # dict 'env_info': env name and its config
+    env_info={"env_id": "pyth_lq", "lq_config": "s4a2"},
 
-    ################################################
-    # Parameters for environment to tested
-    parser.add_argument("--env_info", type=dict, default={"env_id": "pyth_lq", "lq_config": "s4a2"}, help="env info")
-    parser.add_argument("--traj_num", type=int, default=5, help="number of trajectories sampled")
-    parser.add_argument("--init_info", type=dict, default=None, help="initialization info of env")
+    # int 'traj_num': number of trajectories sampled
+    traj_num=3,
 
-    ################################################
-    # Call open-loop checker
-    args = vars(parser.parse_args())
-    check_dynamic(**args)
+    # dict 'init_info': initialization info of env
+    # Option 1: no init_info, reset by default
+        init_info = None,
+    # Option 2: reset by given state
+    #   init_info = {"init_state": [[0.0, 0.0, 0.0, 0.0]]},
+    # Option 3: reset by given state and ref trajectory (e.g. for veh3dofconti env)
+    #   init_info={"init_state": [[0.0, 0.0, 0.0, 1, 0.1, 0.1]], "ref_num": [3]},
+    # Option 4: up to init parameters for each env
+)
