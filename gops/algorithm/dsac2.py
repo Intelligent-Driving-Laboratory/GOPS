acecf10ca3e6a19ceb5ba61a238709cd0f710baf
wenxuanwang <17888819590@163.com>
2023-04-17 16:42:17 +0800

    1. update dsac2 algorithm, add new tricks to improve performacne. 2. change the mlp in ActionCalueDistri to shared mode.

--- gops/algorithm/dsac2.py
+++ gops/algorithm/dsac2.py
@@ -14,8 +14,7 @@
 #  Update: 2021-03-05, Ziqing Gu: create DSAC algorithm
 #  Update: 2021-03-05, Wenxuan Wang: debug DSAC algorithm
 
-__all__ = ["ApproxContainer", "DSAC2"]
-
+__all__=["ApproxContainer","DSAC2"]
 import time
 from copy import deepcopy
 from typing import Tuple
@@ -85,8 +84,6 @@ class DSAC2(AlgorithmBase):
     :param float tau: param for soft update of target network.
     :param bool auto_alpha: whether to adjust temperature automatically.
     :param float alpha: initial temperature.
-    :param float TD_bound: the bound of temporal difference.
-    :param bool bound: whether to bound the q value.
     :param float delay_update: the update delay of the policy network.
     :param Optional[float] target_entropy: target entropy for automatic
         temperature adjustment.
@@ -100,8 +97,6 @@ class DSAC2(AlgorithmBase):
         self.target_entropy = -kwargs["action_dim"]
         self.auto_alpha = kwargs["auto_alpha"]
         self.alpha = kwargs.get("alpha", 0.2)
-        self.TD_bound = kwargs["TD_bound"]
-        self.bound = kwargs["bound"]
         self.delay_update = kwargs["delay_update"]
 
     @property
@@ -111,8 +106,6 @@ class DSAC2(AlgorithmBase):
             "tau",
             "auto_alpha",
             "alpha",
-            "TD_bound",
-            "bound",
             "delay_update",
         )
 
@@ -203,30 +196,28 @@ class DSAC2(AlgorithmBase):
             loss_alpha.backward()
 
         tb_info = {
-            "DSAC/critic_avg_q1-RL iter": q1.item(),
-            "DSAC/critic_avg_q2-RL iter": q2.item(),
-            "DSAC/critic_avg_std1-RL iter": std1.item(),
-            "DSAC/critic_avg_std2-RL iter": std2.item(),
+            "DSAC2/critic_avg_q1-RL iter": q1.item(),
+            "DSAC2/critic_avg_q2-RL iter": q2.item(),
+            "DSAC2/critic_avg_std1-RL iter": std1.item(),
+            "DSAC2/critic_avg_std2-RL iter": std2.item(),
             tb_tags["loss_actor"]: loss_policy.item(),
-            "DSAC/policy_mean-RL iter": policy_mean,
-            "DSAC/policy_std-RL iter": policy_std,
-            "DSAC/entropy-RL iter": entropy.item(),
-            "DSAC/alpha-RL iter": self.__get_alpha(),
+            tb_tags["loss_critic"]: loss_q.item(),
+            "DSAC2/policy_mean-RL iter": policy_mean,
+            "DSAC2/policy_std-RL iter": policy_std,
+            "DSAC2/entropy-RL iter": entropy.item(),
+            "DSAC2/alpha-RL iter": self.__get_alpha(),
             tb_tags["alg_time"]: (time.time() - start_time) * 1000,
         }
 
         return tb_info
 
-    def __q_evaluate(self, obs, act, qnet, use_min=False):
+    def __q_evaluate(self, obs, act, qnet):
         StochaQ = qnet(obs, act)
         mean, log_std = StochaQ[..., 0], StochaQ[..., -1]
         std = log_std.exp()
         normal = Normal(torch.zeros_like(mean), torch.ones_like(std))
-        if use_min:
-            z = -torch.abs(normal.sample())
-        else:
-            z = normal.sample()
-            z = torch.clamp(z, -2, 2)
+        z = normal.sample()
+        z = torch.clamp(z, -3, 3)
         q_value = mean + torch.mul(z, std)
         return mean, std, q_value
 
@@ -242,61 +233,68 @@ class DSAC2(AlgorithmBase):
         act2_dist = self.networks.create_action_distributions(logits_2)
         act2, log_prob_act2 = act2_dist.rsample()
 
-        q1, q1_std, q1_sample = self.__q_evaluate(obs, act, self.networks.q1, use_min=False)
-        q2, q2_std, q2_sample = self.__q_evaluate(obs, act, self.networks.q2, use_min=False)
+        q1, q1_std, _ = self.__q_evaluate(obs, act, self.networks.q1)
+        q2, q2_std, _ = self.__q_evaluate(obs, act, self.networks.q2)
 
         q1_next, _, q1_next_sample = self.__q_evaluate(
-            obs2, act2, self.networks.q1_target, use_min=False
+            obs2, act2, self.networks.q1_target
         )
         
         q2_next, _, q2_next_sample = self.__q_evaluate(
-            obs2, act2, self.networks.q2_target, use_min=False
+            obs2, act2, self.networks.q2_target
         )
         q_next = torch.min(q1_next, q2_next)
         q_next_sample = torch.where(q1_next < q2_next, q1_next_sample, q2_next_sample)
 
         target_q1, target_q1_bound = self.__compute_target_q(
-            rew, done, q1.detach(),q_next.detach(), q_next_sample.detach(), log_prob_act2.detach(),
+            rew, done, q1.detach(),q1_std.detach(), q_next.detach(), q_next_sample.detach(), log_prob_act2.detach(),
         )
         
         target_q2, target_q2_bound = self.__compute_target_q(
-            rew, done, q2.detach(),q_next.detach(), q_next_sample.detach(), log_prob_act2.detach(),
+            rew, done, q2.detach(), q2_std.detach(),q_next.detach(), q_next_sample.detach(), log_prob_act2.detach(),
         )
 
-        if self.bound:
-            q1_loss = torch.mean(
-                torch.pow(q1 - target_q1, 2) / (2 * torch.pow(q1_std.detach(), 2))
-                + torch.pow(q1.detach() - target_q1_bound, 2) / (2 * torch.pow(q1_std, 2))
-                + torch.log(q1_std)
-            )
 
-            q2_loss = torch.mean(
-                torch.pow(q2 - target_q2, 2) / (2 * torch.pow(q2_std.detach(), 2))
-                + torch.pow(q2.detach() - target_q2_bound, 2) / (2 * torch.pow(q2_std, 2))
-                + torch.log(q2_std)
-            )
-        else:
-            q1_loss = -Normal(q1, q1_std).log_prob(target_q1).mean()
-            q2_loss = -Normal(q2, q2_std).log_prob(target_q2).mean()
+        beta1 = min(1/torch.sqrt(torch.mean(torch.pow(q1.detach() - target_q1_bound, 2))),1)
+        # beta1 =1
+
+        q1_loss = torch.mean(
+            torch.pow(q1 - target_q1, 2) / (2 * torch.pow(q1_std.detach(), 2))
+            +beta1 *(torch.pow(q1.detach() - target_q1_bound, 2) / (2 * torch.pow(q1_std, 2))
+            + torch.log(q1_std))
+        )
+
+
+        beta2 = min(1 / torch.sqrt(torch.mean(torch.pow(q2.detach() - target_q2_bound, 2))), 1)
+        # beta2 =1
+
+        q2_loss = torch.mean(
+            torch.pow(q2 - target_q2, 2) / (2 * torch.pow(q2_std.detach(), 2))
+            + beta2*(torch.pow(q2.detach() - target_q2_bound, 2) / (2 * torch.pow(q2_std, 2))
+            + torch.log(q2_std))
+        )
+
 
         return q1_loss +q2_loss, q1.detach().mean(), q2.detach().mean(), q1_std.detach().mean(), q2_std.detach().mean()
 
-    def __compute_target_q(self, r, done, q, q_next, q_next_sample, log_prob_a_next):
+    def __compute_target_q(self, r, done, q,q_std, q_next, q_next_sample, log_prob_a_next):
         target_q = r + (1 - done) * self.gamma * (
             q_next - self.__get_alpha() * log_prob_a_next
         )
         target_q_sample = r + (1 - done) * self.gamma * (
             q_next_sample - self.__get_alpha() * log_prob_a_next
         )
-        
-        difference = torch.clamp(target_q_sample - q, -self.TD_bound, self.TD_bound)
+        td_bound = 3*q_std
+        difference = torch.clamp(target_q_sample - q, -td_bound, td_bound)
+        # if torch.any(torch.abs(target_q_sample - q) > td_bound):
+        #     print("clipping")
         target_q_bound = q + difference
         return target_q.detach(), target_q_bound.detach()
 
     def __compute_loss_policy(self, data: DataDict):
         obs, new_act, new_log_prob = data["obs"], data["new_act"], data["new_log_prob"]
-        q1, _, _ = self.__q_evaluate(obs, new_act, self.networks.q1, use_min=False)
-        q2, _, _ = self.__q_evaluate(obs, new_act, self.networks.q2, use_min=False)
+        q1, _, _ = self.__q_evaluate(obs, new_act, self.networks.q1)
+        q2, _, _ = self.__q_evaluate(obs, new_act, self.networks.q2)
         loss_policy = (self.__get_alpha() * new_log_prob - torch.min(q1,q2)).mean()
         entropy = -new_log_prob.detach().mean()
         return loss_policy, entropy
