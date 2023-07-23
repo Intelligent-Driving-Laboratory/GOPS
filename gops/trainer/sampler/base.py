72eb7ffef73013cd0cd3f188b5ab0780b9e26c8f
yangyujie <yangyj21@mails.tsinghua.edu.cn>
2023-07-23 13:16:01 +0800

    create vector env

--- gops/trainer/sampler/base.py
+++ gops/trainer/sampler/base.py
@@ -17,10 +17,10 @@ import time
 import numpy as np
 import torch
 
-from gymnasium.vector import VectorEnv
+from gops.create_pkg.create_env import create_env
+from gops.env.vector.vector_env import VectorEnv
 from gops.utils.common_utils import set_seed
 from gops.utils.explore_noise import GaussNoise, EpsilonGreedy
-from gops.create_pkg.create_env import create_env
 from gops.utils.tensorboard_setup import tb_tags
 
 
