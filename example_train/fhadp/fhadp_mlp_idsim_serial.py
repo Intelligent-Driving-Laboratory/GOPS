7625d21e72995833903790d1813702b529b4635f
taoletian <taolt1999@outlook.com>
2023-08-16 21:44:34 +0800

    update idsim env & mode config in training script

--- example_train/fhadp/fhadp_mlp_idsim_serial.py
+++ example_train/fhadp/fhadp_mlp_idsim_serial.py
@@ -32,8 +32,7 @@ if __name__ == "__main__":
 
     ################################################
     # Key Parameters for users
-    parser.add_argument("--env_id", type=str, default="pyth_idsim", help="id of environment")
-        MAP_ROOT = 'YOUR_MAP_ROOT'
+    MAP_ROOT = 'YOUR_MAP_ROOT'
     pre_horizon = 30
     env_config_param = {
         "use_render": False,
@@ -131,7 +130,7 @@ if __name__ == "__main__":
     parser.add_argument("--env_config", type=dict, default=env_config_param)
     parser.add_argument("--env_model_config", type=dict, default=model_config)
 
-    parser.add_argument("--algorithm", type=str, default="FHADP2", help="RL algorithm")
+    parser.add_argument("--algorithm", type=str, default="FHADP", help="RL algorithm")
     parser.add_argument("--enable_cuda", default=False, help="Enable CUDA")
     parser.add_argument("--seed", default=2099945076, help="seed")
     parser.add_argument("--pre_horizon", type=int, default=pre_horizon)
@@ -161,7 +160,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--policy_func_name",
         type=str,
-        default="FiniteHorizonFullPolicy",
+        default="FiniteHorizonPolicy",
         help="Options: None/DetermPolicy/FiniteHorizonPolicy/StochaPolicy",
     )
     parser.add_argument(
@@ -174,7 +173,7 @@ if __name__ == "__main__":
         help="Options: default/TanhGaussDistribution/GaussDistribution",
     )
     policy_func_type = parser.parse_known_args()[0].policy_func_type
-    parser.add_argument("--policy_hidden_sizes", type=list, default=[256, 256])
+    parser.add_argument("--policy_hidden_sizes", type=list, default=[64, 64])
     parser.add_argument(
         "--policy_hidden_activation", type=str, default="relu", help="Options: relu/gelu/elu/selu/sigmoid/tanh"
     )
@@ -191,18 +190,12 @@ if __name__ == "__main__":
         "--trainer",
         type=str,
         default="off_serial_trainer",
-        help="Options: on_serial_trainer, on_sync_trainer, off_serial_trainer, off_async_trainer, off_sync_trainer",
+        help="Options: on_serial_trainer, on_sync_trainer, off_serial_trainer, off_async_trainer",
     )
     # Maximum iteration number
-    parser.add_argument("--max_iteration", type=int, default=200000)
+    parser.add_argument("--max_iteration", type=int, default=16000)
     trainer_type = parser.parse_known_args()[0].trainer
     parser.add_argument("--ini_network_dir", type=str, default=None)
-
-    # import ray
-    # ray.init('local')
-    # parser.add_argument("--num_algs", type=int, default=4, help="number of algs")
-    # parser.add_argument("--num_samplers", type=int, default=1, help="number of samplers")
-    # parser.add_argument("--num_buffers", type=int, default=1, help="number of buffers")
 
     # 4.1. Parameters for off_serial_trainer
     parser.add_argument(
@@ -221,24 +214,24 @@ if __name__ == "__main__":
     # 5. Parameters for sampler
     parser.add_argument("--sampler_name", type=str, default="off_sampler", help="Options: on_sampler/off_sampler")
     # Batch size of sampler for buffer store
-    parser.add_argument("--sample_batch_size", type=int, default=128)
+    parser.add_argument("--sample_batch_size", type=int, default=8)
     # Add noise to actions for better exploration
     parser.add_argument("--noise_params", type=dict, default=None)
 
     ################################################
     # 6. Parameters for evaluator
     parser.add_argument("--evaluator_name", type=str, default="evaluator")
-    parser.add_argument("--num_eval_episode", type=int, default=20)
-    parser.add_argument("--eval_interval", type=int, default=1000)
+    parser.add_argument("--num_eval_episode", type=int, default=10)
+    parser.add_argument("--eval_interval", type=int, default=100)
     parser.add_argument("--eval_save", type=str, default=False, help="save evaluation data")
 
     ################################################
     # 7. Data savings
     parser.add_argument("--save_folder", type=str, default=None)
     # Save value/policy every N updates
-    parser.add_argument("--apprfunc_save_interval", type=int, default=10000)
+    parser.add_argument("--apprfunc_save_interval", type=int, default=1000)
     # Save key info every N updates
-    parser.add_argument("--log_save_interval", type=int, default=1000)
+    parser.add_argument("--log_save_interval", type=int, default=200)
 
     ################################################
     # Get parameter dictionary
