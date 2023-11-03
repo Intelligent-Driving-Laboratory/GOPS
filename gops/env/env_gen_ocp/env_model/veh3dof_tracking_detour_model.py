106ecfc8a153b1b9c20936f521305d370f4721ee
Wenjun Zou <929095306@qq.com>
2023-11-03 16:58:35 +0800

    Merge branch '231103' into dev

---
+++ gops/env/env_gen_ocp/env_model/veh3dof_tracking_detour_model.py
@@ -0,0 +1,204 @@
+from typing import Dict, Optional, Tuple, Union
+
+import torch
+import numpy as np
+
+from gops.env.env_gen_ocp.env_model.pyth_base_model import EnvModel
+from gops.env.env_gen_ocp.pyth_base import State
+from gops.env.env_gen_ocp.robot.veh3dof_model import VehDynMdl
+from gops.env.env_gen_ocp.robot.veh3dof import angle_normalize
+
+
+class Veh3DofModel(EnvModel):
+    robot_model: VehDynMdl
+
+    def __init__(
+        self,
+        pre_horizon: int = 10,
+        max_steer: float = torch.pi / 6,
+        device: Union[torch.device, str, None] = None,
+        veh_length: float = 4.8,
+        veh_width: float = 2.0,
+        **kwargs,
+    ):
+        ego_obs_dim = 6
+        ref_obs_dim = 4
+        dt = 0.1
+        super().__init__(
+            obs_dim=ego_obs_dim + ref_obs_dim * pre_horizon + 4,
+            action_dim=2,
+            dt=dt,
+            obs_lower_bound=None,
+            obs_upper_bound=None,
+            action_lower_bound=[-max_steer, -3],
+            action_upper_bound=[max_steer, 3],
+            device=device,
+        )
+        self.robot_model = VehDynMdl(
+            dt=dt,
+            robot_state_dim=ego_obs_dim,
+        )
+        self.pre_horizon = pre_horizon
+        self.veh_length = veh_length
+        self.veh_width = veh_width
+
+    def get_obs(self, state: State) -> torch.Tensor:
+        t = state.context_state.t
+        current_reference = state.context_state.reference[:, t:t + self.pre_horizon + 1]
+        ref_x_tf, ref_y_tf, ref_phi_tf = \
+            ego_vehicle_coordinate_transform(
+                state.robot_state[:, 0],
+                state.robot_state[:, 1],
+                state.robot_state[:, 2],
+                current_reference[..., 0],
+                current_reference[..., 1],
+                current_reference[..., 2],
+            )
+        ref_u_tf = current_reference[..., 3] - state.robot_state[:, 3].unsqueeze(1)
+        ego_obs = torch.concat((torch.stack(
+            (ref_x_tf[:, 0], ref_y_tf[:, 0], ref_phi_tf[:, 0], ref_u_tf[:, 0]), 
+            dim=1
+            ), state.robot_state[:, 4:]), dim=1
+        )
+        ref_obs = torch.stack((ref_x_tf, ref_y_tf, ref_phi_tf, ref_u_tf), 2)[:, 1:] \
+            .reshape(ego_obs.shape[0], -1)
+        
+        current_constraint = state.context_state.constraint[:, t]
+        surr_x_tf, surr_y_tf, surr_phi_tf = ego_vehicle_coordinate_transform(
+            state.robot_state[:, 0],
+            state.robot_state[:, 1],
+            state.robot_state[:, 2],
+            current_constraint[..., 0],
+            current_constraint[..., 1],
+            current_constraint[..., 2],
+        )
+        surr_u_tf = current_constraint[..., 3]
+        surr_obs = torch.stack((surr_x_tf, surr_y_tf, surr_phi_tf, surr_u_tf), 2) \
+            .reshape(ego_obs.shape[0], -1)
+        return torch.concat((ego_obs, ref_obs, surr_obs), 1)
+
+    def get_constraint(self, state: State) -> torch.Tensor:
+        # collision detection using bicircle model
+        # distance from vehicle center to front/rear circle center
+        d = (self.veh_length - self.veh_width) / 2
+        # circle radius
+        r = np.sqrt(2) / 2 * self.veh_width
+        ego_obs = state.robot_state
+        x, y, phi = ego_obs[:, :3].split(1, dim=1)
+        ego_center = torch.stack(
+            (
+                torch.cat((x + d * torch.cos(phi), y + d * torch.sin(phi)), dim=1),
+                torch.cat((x - d * torch.cos(phi), y - d * torch.sin(phi)), dim=1),
+            ),
+            dim=1,
+        )
+
+        surr_state = state.context_state.index_by_t().constraint
+        surr_x, surr_y, surr_phi = surr_state[..., :3].split(1, dim=2)
+        surr_center = torch.stack(
+            (
+                torch.cat(
+                    (
+                        surr_x + d * torch.cos(surr_phi),
+                        surr_y + d * torch.sin(surr_phi),
+                    ),
+                    dim=2,
+                ),
+                torch.cat(
+                    (
+                        surr_x - d * torch.cos(surr_phi),
+                        surr_y - d * torch.sin(surr_phi),
+                    ),
+                    dim=2,
+                ),
+            ),
+            dim=2,
+        )
+
+        min_dist = np.finfo(np.float32).max * torch.ones_like(x)
+        for i in range(2):
+            # front and rear circle of ego vehicle
+            for j in range(2):
+                # front and rear circle of surrounding vehicles
+                dist = torch.linalg.norm(
+                    ego_center[:, i].unsqueeze(1) - surr_center[..., j, :], dim=2
+                )
+                min_dist = torch.minimum(
+                    min_dist, torch.min(dist, dim=1, keepdim=True).values
+                )
+
+        ego_to_veh_violation = 2 * r - min_dist
+        # # road boundary violation
+        # ego_upper_y = ego_center[:, :, 1] + r
+        # ego_lower_y = ego_center[:, :, 1] - r
+        # upper_bound_violation = torch.max(ego_upper_y - self.upper_bound, dim=1, keepdim=True).values
+        # lower_bound_violation = torch.max(self.lower_bound - ego_lower_y, dim=1, keepdim=True).values
+        # constraint = torch.cat(
+        #     (ego_to_veh_violation, upper_bound_violation, lower_bound_violation), dim=1
+        # )
+        return ego_to_veh_violation
+    
+    def get_reward(self, state: State, action: torch.Tensor) -> torch.Tensor:
+        ego_obs = state.robot_state
+        x, y, phi, u, w = ego_obs[:, 0], ego_obs[:, 1], ego_obs[:, 2], ego_obs[:, 3], ego_obs[:, 5]
+        ref_obs = state.context_state.index_by_t().reference
+        ref_x, ref_y, ref_phi, ref_u = ref_obs[:, 0], ref_obs[:, 1], ref_obs[:, 2], ref_obs[:, 3]
+        steer, a_x = action[:, 0], action[:, 1]
+        reward = - 0.01 * (
+            10.0 * (x - ref_x) ** 2
+            + 10.0 * (y - ref_y) ** 2
+            + 500 * angle_normalize(phi - ref_phi) ** 2
+            + 5.0 * (u - ref_u) ** 2
+            + 1000 * w ** 2
+            + 1000  * steer ** 2
+            + 50  * a_x ** 2
+        )
+        return reward
+
+    def get_terminated(self, state: State) -> torch.bool:
+        ego_obs = state.robot_state
+        x, y, phi = ego_obs[:, 0], ego_obs[:, 1], ego_obs[:, 2]
+        ref_obs = state.context_state.index_by_t().reference
+        ref_x, ref_y, ref_phi = ref_obs[:, 0], ref_obs[:, 1], ref_obs[:, 2]
+        done = (
+            (torch.abs(x - ref_x) > 5)
+            | (torch.abs(y - ref_y) > 2)
+            | (torch.abs(angle_normalize(phi - ref_phi)) > torch.pi)
+        )
+        return done
+    
+    def forward(self, obs, action, done, info):
+        state = info["state"]
+        next_state = self.get_next_state(state, action)
+        next_obs = self.get_obs(next_state)
+        reward = self.get_reward(state, action)
+        terminated = self.get_terminated(state)
+        constraint = self.get_constraint(state)
+        next_info = {}
+        next_info["state"] = next_state
+        next_info["constraint"] = constraint
+        return next_obs, reward, terminated, next_info
+
+
+def ego_vehicle_coordinate_transform(
+    ego_x: torch.Tensor,
+    ego_y: torch.Tensor,
+    ego_phi: torch.Tensor,
+    ref_x: torch.Tensor,
+    ref_y: torch.Tensor,
+    ref_phi: torch.Tensor,
+) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    ego_x, ego_y, ego_phi = ego_x.unsqueeze(1), ego_y.unsqueeze(1), ego_phi.unsqueeze(1)
+    cos_tf = torch.cos(-ego_phi)
+    sin_tf = torch.sin(-ego_phi)
+    ref_x_tf = (ref_x - ego_x) * cos_tf - (ref_y - ego_y) * sin_tf
+    ref_y_tf = (ref_x - ego_x) * sin_tf + (ref_y - ego_y) * cos_tf
+    ref_phi_tf = angle_normalize(ref_phi - ego_phi)
+    return ref_x_tf, ref_y_tf, ref_phi_tf
+
+
+def env_model_creator(**kwargs) -> Veh3DofModel:
+    """
+    make env model `veh3dof_tracking`
+    """
+    return Veh3DofModel(**kwargs)
--- gops/env/env_gen_ocp/veh3dof_tracking_detour.py
+++ gops/env/env_gen_ocp/veh3dof_tracking_detour.py
@@ -1,13 +1,14 @@
+from dataclasses import dataclass, fields
 from typing import Dict, Optional, Sequence, Tuple
 
 import numpy as np
 from gym import spaces
 from gops.env.env_gen_ocp.pyth_base import Env, State
 from gops.env.env_gen_ocp.robot.veh3dof import Veh3DoF, angle_normalize
-from gops.env.env_gen_ocp.context.ref_traj import RefTrajContext
+from gops.env.env_gen_ocp.context.ref_traj_with_static_obstacle import RefTrajWithStaticObstacleContext
+from gops.env.env_gen_ocp.veh3dof_tracking import Veh3DoFTracking
 
-
-class Veh3DoFTracking(Env):
+class Veh3DoFTrackingDetour(Veh3DoFTracking):
     def __init__(
         self,
         *,
@@ -19,36 +20,33 @@ class Veh3DoFTracking(Env):
         max_steer: float = np.pi / 6,
         **kwargs,
     ):
-        self.robot: Veh3DoF = Veh3DoF(
+        super().__init__(
+            pre_horizon=pre_horizon,
             dt=dt,
+            path_para=path_para,
+            u_para=u_para,
             max_acc=max_acc,
             max_steer=max_steer,
+            **kwargs,
         )
-        self.context: RefTrajContext = RefTrajContext(
+        self.init_high = np.array([1, 0.0, np.pi / 36, 2, 0.1, 0.1], dtype=np.float32)
+        self.init_low = -np.array([1, 0.8, np.pi / 36, 2, 0.1, 0.1], dtype=np.float32)
+
+        self.context: RefTrajWithStaticObstacleContext = RefTrajWithStaticObstacleContext(
             pre_horizon=pre_horizon,
             dt=dt,
             path_param=path_para,
             speed_param=u_para,
         )
-
         self.state_dim = 6
         ego_obs_dim = 6
         ref_obs_dim = 4
+        veh_obs_dim = 4
         self.observation_space = spaces.Box(
-            low=np.array([-np.inf] * (ego_obs_dim + ref_obs_dim * pre_horizon)),
-            high=np.array([np.inf] * (ego_obs_dim + ref_obs_dim * pre_horizon)),
+            low=np.array([-np.inf] * (ego_obs_dim + ref_obs_dim * pre_horizon + veh_obs_dim * self.context.surr_veh_num)),
+            high=np.array([np.inf] * (ego_obs_dim + ref_obs_dim * pre_horizon + veh_obs_dim * self.context.surr_veh_num)),
             dtype=np.float32,
         )
-        self.action_space = self.robot.action_space
-        self.dt = dt
-        self.pre_horizon = pre_horizon
-
-        self.max_episode_steps = 200
-
-        self.init_high = np.array([2, 1, np.pi / 6, 2, 0.1, 0.1], dtype=np.float32)
-        self.init_low = -self.init_high
-
-        self.seed()
 
     def reset(
         self,
@@ -56,93 +54,111 @@ class Veh3DoFTracking(Env):
         options: Optional[dict] = None,
         init_state: Optional[Sequence] = None,
         ref_time: Optional[float] = None,
-        ref_num: Optional[int] = None,
+        ref_num: Optional[int] = 9,
     ) -> Tuple[np.ndarray, dict]:
-        if seed is not None:
-            self.seed(seed)
+        return super().reset(
+            seed=seed,
+            options=options,
+            init_state=init_state,
+            ref_time=ref_time,
+            ref_num=9,
+        )
+    
+    def _get_constraint(self) -> np.ndarray:
+        # collision detection using bicircle model
+        # distance from vehicle center to front/rear circle center
+        veh_length = self.context.veh_length
+        veh_width = self.context.veh_width
+        d = (veh_length - veh_width) / 2
+        # circle radius
+        r = 0.5 * veh_width
 
-        if ref_time is None:
-            ref_time = 20.0 * self.np_random.uniform(0.0, 1.0)
-
-        # Calculate path num and speed num: ref_num = [0, 1, 2,..., 7]
-        if ref_num is None:
-            path_num = None
-            speed_num = None
-        else:
-            path_num = int(ref_num / 2)
-            speed_num = int(ref_num % 2)
-
-        # If no ref_num, then randomly select path and speed
-        if path_num is None:
-            path_num = self.np_random.choice([0, 1, 2, 3])
-        if speed_num is None:
-            speed_num = self.np_random.choice([0, 1])
-
-        context_state = self.context.reset(
-            ref_time=ref_time, path_num=path_num, speed_num=speed_num)
-
-        if init_state is None:
-            delta_state = self.np_random.uniform(low=self.init_low, high=self.init_high).astype(np.float32)
-        else:
-            delta_state = np.array(init_state, dtype=np.float32)
-        init_state = np.concatenate(
-            (context_state.reference[0] + delta_state[:4], delta_state[4:])
+        ego_x, ego_y, ego_phi = self.robot.state[:3]
+        ego_center = np.array(
+            [
+                [ego_x + d * np.cos(ego_phi), ego_y + d * np.sin(ego_phi)],
+                [ego_x - d * np.cos(ego_phi), ego_y - d * np.sin(ego_phi)],
+            ],
+            dtype=np.float32,
+        )
+        surr_x, surr_y, surr_phi = self.context.state.constraint[0, :, :3].T
+        surr_center = np.stack(
+            (
+                np.stack(
+                    ((surr_x + d * np.cos(surr_phi)), surr_y + d * np.sin(surr_phi)),
+                    axis=1,
+                ),
+                np.stack(
+                    ((surr_x - d * np.cos(surr_phi)), surr_y - d * np.sin(surr_phi)),
+                    axis=1,
+                ),
+            ),
+            axis=1,
         )
 
-        self._state = State(
-            robot_state=self.robot.reset(init_state),
-            context_state=context_state,
-        )
-        return self._get_obs(), self._get_info()
+        min_dist = np.inf
+        for i in range(2):
+            # front and rear circle of ego vehicle
+            for j in range(2):
+                # front and rear circle of surrounding vehicles
+                dist = np.linalg.norm(
+                    ego_center[np.newaxis, i] - surr_center[:, j], axis=1
+                )
+                min_dist = min(min_dist, np.min(dist))
+        ego_to_veh_violation = 2 * r - min_dist
+
+        return np.array([ego_to_veh_violation], dtype=np.float32)
 
     def _get_obs(self) -> np.ndarray:
-        ref_x_tf, ref_y_tf, ref_phi_tf = \
-            ego_vehicle_coordinate_transform(
-                self.robot.state[0],
-                self.robot.state[1],
-                self.robot.state[2],
-                self.context.state.reference[:self.pre_horizon + 1, 0],
-                self.context.state.reference[:self.pre_horizon + 1, 1],
-                self.context.state.reference[:self.pre_horizon + 1, 2],
-            )
-        ref_u_tf = self.context.state.reference[:self.pre_horizon + 1, 3] - self.robot.state[3]
-        # ego_obs: [
-        # delta_x, delta_y, delta_phi, delta_u, (of the first reference point)
-        # v, w (of ego vehicle)
-        # ]
-        ego_obs = np.concatenate((
-            [ref_x_tf[0], ref_y_tf[0], ref_phi_tf[0], ref_u_tf[0]],
-            self.robot.state[4:],
-        ))
-        # ref_obs: [
-        # delta_x, delta_y, delta_phi, delta_u (of the second to last reference point)
-        # ]
-        ref_obs = np.stack((ref_x_tf, ref_y_tf, ref_phi_tf, ref_u_tf), 1)[1:].flatten()
-        return np.concatenate((ego_obs, ref_obs))
+        obs = super()._get_obs()
+        # surr_obs
+        surr_x_tf, surr_y_tf, surr_phi_tf = ego_vehicle_coordinate_transform(
+            self.robot.state[0],
+            self.robot.state[1],
+            self.robot.state[2],
+            self.context.state.constraint[0, :, 0],
+            self.context.state.constraint[0, :, 1],
+            self.context.state.constraint[0, :, 2],
+        )
+        surr_obs_rel = np.concatenate((surr_x_tf, surr_y_tf, surr_phi_tf, self.context.state.constraint[0, :, 3]))
+        return np.concatenate((obs, surr_obs_rel))
 
     def _get_reward(self, action: np.ndarray) -> float:
         x, y, phi, u, _, w = self.robot.state
         ref_x, ref_y, ref_phi, ref_u = self.context.state.reference[0]
         steer, a_x = action
-        return -(
-            0.04 * (x - ref_x) ** 2
-            + 0.04 * (y - ref_y) ** 2
-            + 0.02 * angle_normalize(phi - ref_phi) ** 2
-            + 0.02 * (u - ref_u) ** 2
-            + 0.01 * w ** 2
-            + 0.01 * steer ** 2
-            + 0.01 * a_x ** 2
-        )
+        violation = self._get_constraint()
+        threshold = -0.1
+        punish = np.maximum(violation - threshold, 0).sum()
+        if (punish > 0) :
+            punish += 1.0
+        done = self._get_terminated()
+        return - 0.01 * (
+            10.0 * (x - ref_x) ** 2
+            + 10.0 * (y - ref_y) ** 2
+            + 500 * angle_normalize(phi - ref_phi) ** 2
+            + 5.0 * (u - ref_u) ** 2
+            + 1000 * w ** 2
+            + 1000  * steer ** 2
+            + 50  * a_x ** 2
+            + 500.0 * punish
+        ) + 2.0 - 100 * done
 
     def _get_terminated(self) -> bool:
         x, y, phi = self.robot.state[:3]
         ref_x, ref_y, ref_phi = self.context.state.reference[0, :3]
         done = (
             (np.abs(x - ref_x) > 5)
-            | (np.abs(y - ref_y) > 2)
+            | (np.abs(y - ref_y) > 3)
             | (np.abs(angle_normalize(phi - ref_phi)) > np.pi)
         )
         return done
+
+    def _get_info(self) -> dict:
+        return {
+            **super()._get_info(),
+            "constraint": self._get_constraint().copy(),
+        }
 
     def render(self, mode="human"):
         import matplotlib.pyplot as plt
@@ -191,10 +207,10 @@ class Veh3DoFTracking(Env):
         ref_y = []
         for i in np.arange(1, self.context.pre_horizon + 1):
             ref_x.append(self.context.ref_traj.compute_x(
-                self.context.ref_time + i * self.dt, self.context.path_num, self.context.speed_num
+                self.context.ref_time + i * self.context.dt, self.context.path_num, self.context.speed_num
             ))
             ref_y .append(self.context.ref_traj.compute_y(
-                self.context.ref_time + i * self.dt, self.context.path_num, self.context.speed_num
+                self.context.ref_time + i * self.context.dt, self.context.path_num, self.context.speed_num
             ))
         ax.plot(ref_x, ref_y, 'b--', lw=1, zorder=2)
 
@@ -208,6 +224,24 @@ class Veh3DoFTracking(Env):
         ax.text(left_x, top_y - delta_y, f'speed: {ego_speed:.1f}km/h')
         ax.text(left_x, top_y - 2 * delta_y, f'ref speed: {ref_speed:.1f}km/h')
 
+        # draw surrounding vehicles
+        veh_length = self.context.veh_length
+        veh_width = self.context.veh_width
+        for i in range(self.context.surr_veh_num):
+            surr_x, surr_y, surr_phi = self.context.state.constraint[0, i, :3]
+            rectan_x = surr_x - veh_length / 2 * np.cos(surr_phi) + veh_width / 2 * np.sin(surr_phi)
+            rectan_y = surr_y - veh_width / 2 * np.cos(surr_phi) - veh_length / 2 * np.sin(surr_phi)
+            ax.add_patch(pc.Rectangle(
+                (rectan_x, rectan_y), self.context.veh_length, self.context.veh_width, angle=surr_phi * 180 / np.pi,
+                facecolor='w', edgecolor='k', zorder=1))
+
+       # render self.upper_bound and self.lower_bound with solid line
+        upper_x = np.linspace(-100, 200, 100)
+        lower_x = upper_x
+        upper_y = np.ones_like(upper_x) * self.context.upper_bound
+        lower_y = np.ones_like(lower_x) * self.context.lower_bound
+        ax.plot(upper_x, upper_y, "k")
+        ax.plot(lower_x, lower_y, "k")
 
 def ego_vehicle_coordinate_transform(
     ego_x: np.ndarray,
@@ -242,19 +276,18 @@ def ego_vehicle_coordinate_transform(
 
 
 def env_creator(**kwargs):
-    return Veh3DoFTracking(**kwargs)
-
+    return Veh3DoFTrackingDetour(**kwargs)
 
 if __name__ == "__main__":
     # test consistency with old environment
 
     import numpy as np
-    from gops.env.env_gen_ocp.veh3dof_tracking import Veh3DoFTracking
-    from gops.env.env_ocp.pyth_veh3dofconti import SimuVeh3dofconti
+    from gops.env.env_gen_ocp.veh3dof_tracking_detour import Veh3DoFTrackingDetour
+    from gops.env.env_ocp.pyth_veh3dofconti_detour import SimuVeh3dofcontiSurrCstr
 
 
-    env_old = SimuVeh3dofconti()
-    env_new = Veh3DoFTracking()
+    env_old = SimuVeh3dofcontiSurrCstr()
+    env_new = Veh3DoFTrackingDetour()
 
     seed = 1
     env_old.seed(seed)
