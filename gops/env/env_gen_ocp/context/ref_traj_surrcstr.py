7169f9fa1aa7fda085d5c3d02d76d5cc658e58aa
Wenjun Zou <929095306@qq.com>
2023-11-21 17:52:09 +0800

    Merge branch '20231121' into dev

---
+++ gops/env/env_gen_ocp/context/ref_traj_surrcstr.py
@@ -0,0 +1,145 @@
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional, Sequence, Tuple
+
+import numpy as np
+import copy
+from gops.env.env_gen_ocp.pyth_base import ContextState
+from gops.env.env_gen_ocp.context.ref_traj import RefTrajContext
+from gops.env.env_ocp.env_model.pyth_veh3dofconti_model import angle_normalize
+
+@dataclass
+class SurrVehicleData:
+    x: float = 0.0
+    y: float = 0.0
+    phi: float = 0.0
+    u: float = 0.0
+    # front wheel angle
+    delta: float = 0.0
+    # distance from front axle to rear axle
+    l: float = 3.0
+    dt: float = 0.1
+
+    def step(self):
+        self.x = self.x + self.u * np.cos(self.phi) * self.dt
+        self.y = self.y + self.u * np.sin(self.phi) * self.dt
+        self.phi = self.phi + self.u * np.tan(self.delta) / self.l * self.dt
+        self.phi = angle_normalize(self.phi)
+    
+class RefTrajSurrCstrContext(RefTrajContext):
+    def __init__(
+        self,
+        *,
+        pre_horizon: int = 10,
+        surr_veh_num: int = 4,
+        dt: float = 0.1,
+        path_param: Optional[Dict[str, Dict]] = None,
+        speed_param: Optional[Dict[str, Dict]] = None,
+    ):
+        super().__init__(
+            pre_horizon=pre_horizon,
+            dt=dt,
+            path_param=path_param,
+            speed_param=speed_param,
+        )
+        self.surr_veh_num = surr_veh_num
+        self.veh_length = 4.8
+        self.veh_width = 2.0
+        self.surr_vehs: List[SurrVehicleData] = None
+
+        self.lane_width = 4.0
+        self.upper_bound = 0.5 * self.lane_width
+        self.lower_bound = -1.5 * self.lane_width
+
+    def reset(
+        self,
+        *,
+        ref_time: float,
+        path_num: Optional[int] = None,
+        speed_num: Optional[int] = None,
+        np_random: Optional[np.random.RandomState] = None,
+    ) -> ContextState[np.ndarray]:
+        self.state = super().reset(
+            ref_time=ref_time,
+            path_num=path_num,
+            speed_num=speed_num,
+        )
+
+        ref_points = self.state.reference
+
+        surr_x0, surr_y0 = ref_points[0, :2]
+        if path_num == 3:
+            # circle path
+            surr_phi = ref_points[0, 2]
+            surr_delta = -np.arctan2(SurrVehicleData.l, self.ref_traj.ref_trajs[3].r)
+        else:
+            surr_phi = 0.0
+            surr_delta = 0.0
+
+        self.surr_vehs = []
+        for _ in range(self.surr_veh_num):
+            # avoid ego vehicle
+            while True:
+                # TODO: sample position according to reference trajectory
+                delta_lon = 10 * np_random.uniform(-1, 1)
+                delta_lat = 5 * np_random.uniform(-1, 1)
+                if abs(delta_lon) > 7 or abs(delta_lat) > 3:
+                    break
+            surr_x = (
+                surr_x0 + delta_lon * np.cos(surr_phi) - delta_lat * np.sin(surr_phi)
+            )
+            surr_y = (
+                surr_y0 + delta_lon * np.sin(surr_phi) + delta_lat * np.cos(surr_phi)
+            )
+            surr_u = 5 + np_random.uniform(-1, 1)
+            self.surr_vehs.append(
+                SurrVehicleData(
+                    x=surr_x,
+                    y=surr_y,
+                    phi=surr_phi,
+                    u=surr_u,
+                    delta=surr_delta,
+                    dt=self.dt,
+                )
+            )
+        # self.update_surr_state()
+
+        self.state = ContextState(
+            reference=ref_points,
+            constraint=self.get_surr_state_pred(),
+        )
+        return self.state
+    
+    def step_surr(self, ):
+        for surr_veh in self.surr_vehs:
+            surr_veh.step()
+    
+    def get_surr_state(self):
+        surr_state = np.zeros((self.surr_veh_num, 5), dtype=np.float32)
+        for i, surr_veh in enumerate(self.surr_vehs):
+            surr_state[i] = np.array(
+                [surr_veh.x, surr_veh.y, surr_veh.phi, surr_veh.u, surr_veh.delta],
+                dtype=np.float32,
+            )
+        return surr_state
+    
+    def get_surr_state_pred(self):
+        surr_state_pred = np.zeros((self.pre_horizon + 1, self.surr_veh_num, 5), dtype=np.float32)
+        surr_state_pred[0] = self.get_surr_state()
+        surr_vehs_backup = copy.deepcopy(self.surr_vehs)
+        for i in range(self.pre_horizon):
+            self.step_surr()
+            surr_state_pred[i + 1] = self.get_surr_state()
+        self.surr_vehs = surr_vehs_backup
+        return surr_state_pred
+
+    def step(self) -> ContextState[np.ndarray]:
+        super().step()
+        self.step_surr()
+        self.state.constraint = self.get_surr_state_pred()
+
+        return self.state
+
+    def get_zero_state(self) -> ContextState[np.ndarray]:
+        context_state = super().get_zero_state()
+        context_state.constraint = np.zeros((self.pre_horizon + 1, self.surr_veh_num, 5), dtype=np.float32)
+        return context_state
---
+++ gops/env/env_gen_ocp/veh3dof_tracking_surrcstr.py
@@ -0,0 +1,191 @@
+from typing import Dict, Optional, Sequence, Tuple
+
+import numpy as np
+from gym import spaces
+from gops.env.env_gen_ocp.robot.veh3dof import angle_normalize
+from gops.env.env_gen_ocp.context.ref_traj_surrcstr import RefTrajSurrCstrContext
+from gops.env.env_gen_ocp.veh3dof_tracking import Veh3DoFTracking, ego_vehicle_coordinate_transform
+
+class Veh3DoFTrackingSurrCstr(Veh3DoFTracking):
+    def __init__(
+        self,
+        *,
+        pre_horizon: int = 10,
+        dt: float = 0.1,
+        path_para: Optional[Dict[str, Dict]] = None,
+        u_para: Optional[Dict[str, Dict]] = None,
+        max_acc: float = 3.0,
+        max_steer: float = np.pi / 6,
+        **kwargs,
+    ):
+        super().__init__(
+            pre_horizon=pre_horizon,
+            dt=dt,
+            path_para=path_para,
+            u_para=u_para,
+            max_acc=max_acc,
+            max_steer=max_steer,
+            **kwargs,
+        )
+        self.init_high = np.array([2, 1, np.pi / 6, 2, 0.1, 0.1], dtype=np.float32)
+        self.init_low = -self.init_high
+
+        self.context: RefTrajSurrCstrContext = RefTrajSurrCstrContext(
+            pre_horizon=pre_horizon,
+            dt=dt,
+            path_param=path_para,
+            speed_param=u_para,
+        )
+        self.state_dim = 6
+        ego_obs_dim = 6
+        ref_obs_dim = 4
+        veh_obs_dim = 4
+        self.observation_space = spaces.Box(
+            low=np.array([-np.inf] * (ego_obs_dim + ref_obs_dim * pre_horizon + veh_obs_dim * self.context.surr_veh_num)),
+            high=np.array([np.inf] * (ego_obs_dim + ref_obs_dim * pre_horizon + veh_obs_dim * self.context.surr_veh_num)),
+            dtype=np.float32,
+        )
+        self.max_episode_steps = 100
+
+    def reset(
+        self,
+        seed: Optional[int] = None,
+        options: Optional[dict] = None,
+        init_state: Optional[Sequence] = None,
+        ref_time: Optional[float] = None,
+        ref_num: Optional[int] = None,
+    ) -> Tuple[np.ndarray, dict]:
+        return super().reset(
+            seed=seed,
+            options=options,
+            init_state=init_state,
+            ref_time=ref_time,
+            ref_num=ref_num,
+        )
+    
+    def _get_constraint(self) -> np.ndarray:
+        # collision detection using bicircle model
+        # distance from vehicle center to front/rear circle center
+        veh_length = self.context.veh_length
+        veh_width = self.context.veh_width
+        d = (veh_length - veh_width) / 2
+        # circle radius
+        r = np.sqrt(2) / 2 * veh_width
+
+        ego_x, ego_y, ego_phi = self.robot.state[:3]
+        ego_center = np.array(
+            [
+                [ego_x + d * np.cos(ego_phi), ego_y + d * np.sin(ego_phi)],
+                [ego_x - d * np.cos(ego_phi), ego_y - d * np.sin(ego_phi)],
+            ],
+            dtype=np.float32,
+        )
+        surr_x, surr_y, surr_phi = self.context.state.constraint[0, :, :3].T
+        surr_center = np.stack(
+            (
+                np.stack(
+                    ((surr_x + d * np.cos(surr_phi)), surr_y + d * np.sin(surr_phi)),
+                    axis=1,
+                ),
+                np.stack(
+                    ((surr_x - d * np.cos(surr_phi)), surr_y - d * np.sin(surr_phi)),
+                    axis=1,
+                ),
+            ),
+            axis=1,
+        )
+        # print("new ego_center: ", ego_center)
+        # print("new surr_center: ", surr_center)
+
+        min_dist = np.inf
+        for i in range(2):
+            # front and rear circle of ego vehicle
+            for j in range(2):
+                # front and rear circle of surrounding vehicles
+                dist = np.linalg.norm(
+                    ego_center[np.newaxis, i] - surr_center[:, j], axis=1
+                )
+                min_dist = min(min_dist, np.min(dist))
+        ego_to_veh_violation = 2 * r - min_dist
+        # print("new ego_to_veh_violation: ", ego_to_veh_violation)
+        return np.array([ego_to_veh_violation], dtype=np.float32)
+
+    def _get_obs(self) -> np.ndarray:
+        obs = super()._get_obs()
+        # surr_obs
+        surr_x_tf, surr_y_tf, surr_phi_tf = ego_vehicle_coordinate_transform(
+            self.robot.state[0],
+            self.robot.state[1],
+            self.robot.state[2],
+            self.context.state.constraint[0, :, 0],
+            self.context.state.constraint[0, :, 1],
+            self.context.state.constraint[0, :, 2],
+        )
+        surr_obs_rel = np.concatenate((surr_x_tf, surr_y_tf, surr_phi_tf, self.context.state.constraint[0, :, 3]))
+        return np.concatenate((obs, surr_obs_rel))
+
+    def _get_reward(self, action: np.ndarray) -> float:
+        x, y, phi, u, _, w = self.robot.state
+        ref_x, ref_y, ref_phi, ref_u = self.context.state.reference[0]
+        steer, a_x = action
+        # violation = self._get_constraint()
+        # threshold = -0.1
+        # punish = np.maximum(violation - threshold, 0).sum()
+        # if punish > 0:
+        #     punish += 1.0
+        return -(
+            0.04 * (x - ref_x) ** 2
+            + 0.04 * (y - ref_y) ** 2
+            + 0.02 * angle_normalize(phi - ref_phi) ** 2
+            + 0.02 * (u - ref_u) ** 2
+            + 0.01 * w ** 2
+            + 0.01 * steer ** 2
+            + 0.01 * a_x ** 2
+        ) - 100 * self._get_terminated()
+
+    def _get_terminated(self) -> bool:
+        x, y, phi = self.robot.state[:3]
+        ref_x, ref_y, ref_phi = self.context.state.reference[0, :3]
+        done = (
+            (np.abs(x - ref_x) > 5)
+            | (np.abs(y - ref_y) > 2)
+            | (np.abs(angle_normalize(phi - ref_phi)) > np.pi)
+        )
+        return done
+
+    def _render(self, ax, veh_length=4.8, veh_width=2.0):
+        import matplotlib.patches as pc
+        super()._render(ax, veh_length, veh_width)
+
+        # draw surrounding vehicles
+        veh_length = self.context.veh_length
+        veh_width = self.context.veh_width
+        for i in range(self.context.surr_veh_num):
+            surr_x, surr_y, surr_phi = self.context.state.constraint[0, i, :3]
+            rectan_x = surr_x - veh_length / 2 * np.cos(surr_phi) + veh_width / 2 * np.sin(surr_phi)
+            rectan_y = surr_y - veh_width / 2 * np.cos(surr_phi) - veh_length / 2 * np.sin(surr_phi)
+            ax.add_patch(pc.Rectangle(
+                (rectan_x, rectan_y), self.context.veh_length, self.context.veh_width, angle=surr_phi * 180 / np.pi,
+                facecolor='w', edgecolor='k', zorder=1))
+
+       # render self.upper_bound and self.lower_bound with solid line
+        upper_x = np.linspace(-100, 200, 100)
+        lower_x = upper_x
+        upper_y = np.ones_like(upper_x) * self.context.upper_bound
+        lower_y = np.ones_like(lower_x) * self.context.lower_bound
+        ax.plot(upper_x, upper_y, "k")
+        ax.plot(lower_x, lower_y, "k")
+
+
+def env_creator(**kwargs):
+    return Veh3DoFTrackingSurrCstr(**kwargs)
+
+
+if __name__ == "__main__":
+    from gops.env.env_ocp.pyth_veh3dofconti_surrcstr import SimuVeh3dofcontiSurrCstr
+    from gops.env.inspector.consistency_checker import check_env_old_new_consistency
+
+    env_old = SimuVeh3dofcontiSurrCstr()
+    env_new = Veh3DoFTrackingSurrCstr()
+
+    check_env_old_new_consistency(env_old, env_new)
